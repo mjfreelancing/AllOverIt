c449fb65baa545d6ca2f354dbd733ac67a20ff0a
mjfreelancing <malcolm@mjfreelancing.com>
2021-07-07 16:07:20 +1000

    Adding lost ObjectPropertySerializationHelper (with updates) and a demo

---
+++ Demos/AllOverIt/SerializeObjectProperties/Program.cs
@@ -0,0 +1,134 @@
+﻿using AllOverIt.Extensions;
+using AllOverIt.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SerializeObjectProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serializer = new ObjectPropertySerializationHelper { IncludeEmptyCollections = true, IncludeNulls = true };
+
+            SerializeObject(serializer);
+
+            Console.WriteLine();
+            SerializeDictionary(serializer);
+
+            Console.WriteLine();
+            SerializeList(serializer);
+
+            Console.WriteLine("");
+            Console.WriteLine("All Over It.");
+            Console.ReadKey();
+        }
+
+        private static void SerializeObject(ObjectPropertySerializationHelper serializer)
+        {
+            var metadataChild = new
+            {
+                Prop1 = 1,
+                Prop2 = false,
+                Prop3 = new
+                {
+                    Prop4 = "Hello",
+                    Prop5 = new List<int>
+                    {
+                        1, 2, 3, 4
+                    },
+                    Prop6 = new Dummy
+                    {
+                        Prop7 = "World",
+                        Prop8 = 11,
+                        Prop9 = 2.3d,
+                        Prop10 = true,
+                        Prop11 = new Dummy
+                        {
+                            //
+                        }
+                    }
+                },
+                Prop12 = new List<int> { -1, -2, -3 }
+            };
+
+            var metadataRoot = new
+            {
+                Prop13 = "Root",
+                Prop14 = metadataChild.ToPropertyDictionary(), // of type IDictionary<string, object>
+                Prop15 = new Dictionary<int, string>
+                {
+                    { 1, "one" }, { 2, "two" }, { 3, "three" }
+                },
+                Prop16 = new Dictionary<Dummy, int>
+                {
+                    { new Dummy(), 1 },
+                    { new Dummy(), 2 }
+                },
+                Prop17 = new List<int>(),
+                Prop18 = new Dictionary<string, object>
+                {
+                    {"Some Key", new Dictionary<int, string>
+                    {
+                        { 1, "one" }, { 2, "two" }, { 3, "three" }
+                    }}
+                },
+                Prop19 = new List<Action<int>>
+                {
+                    _ => { }
+                },
+                Prop20 = new List<Func<int, string, int>>
+                {
+                    (_, _) => 1
+                },
+                prop21 = (bool?)null
+            };
+
+            Console.WriteLine("Object serialization values:");
+            Console.WriteLine("============================");
+
+            var items = serializer.SerializeToDictionary(metadataRoot).Select(kvp => $"{kvp.Key} = {kvp.Value}");
+
+            foreach (var item in items)
+            {
+                Console.WriteLine($"  {item}");
+            }
+
+        }
+
+        private static void SerializeDictionary(ObjectPropertySerializationHelper serializer)
+        {
+            var dictionary = new Dictionary<string, int>
+            {
+                { "one", 1 },
+                { "two", 2 },
+                { "three", 3 },
+                { "four", 4 }
+            };
+
+            Console.WriteLine("Dictionary serialization values:");
+            Console.WriteLine("================================");
+
+            var items2 = serializer.SerializeToDictionary(dictionary).Select(kvp => $"{kvp.Key} = {kvp.Value}");
+            foreach (var item in items2)
+            {
+                Console.WriteLine($"  {item}");
+            }
+        }
+
+        private static void SerializeList(ObjectPropertySerializationHelper serializer)
+        {
+            var list = Enumerable.Range(1, 4).Select(value => $"Value {value}").ToList();
+
+            Console.WriteLine("List serialization values:");
+            Console.WriteLine("==========================");
+
+            var items3 = serializer.SerializeToDictionary(list).Select(kvp => $"{kvp.Key} = {kvp.Value}");
+            foreach (var item in items3)
+            {
+                Console.WriteLine($"  {item}");
+            }
+        }
+    }
+}
---
+++ Source/AllOverIt/Helpers/ObjectPropertySerializationHelper.cs
@@ -0,0 +1,226 @@
+﻿using AllOverIt.Extensions;
+using AllOverIt.Reflection;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllOverIt.Helpers
+{
+    public sealed class ObjectPropertySerializationHelper
+    {
+        internal readonly List<Type> IgnoredTypes = new()
+        {
+            typeof(Task),
+            typeof(Action),
+            typeof(Action<>),
+            typeof(Action<,>),
+            typeof(Action<,,>),
+            typeof(Action<,,,>),
+            typeof(Action<,,,,>),
+            typeof(Action<,,,,,>),
+            typeof(Action<,,,,,,>),
+            typeof(Action<,,,,,,,>),
+            typeof(Action<,,,,,,,,>),
+            typeof(Action<,,,,,,,,,>),
+            typeof(Action<,,,,,,,,,,>),
+            typeof(Action<,,,,,,,,,,,>),
+            typeof(Action<,,,,,,,,,,,,>),
+            typeof(Action<,,,,,,,,,,,,,>),
+            typeof(Action<,,,,,,,,,,,,,,>),
+            typeof(Action<,,,,,,,,,,,,,,,>),
+            typeof(Func<>),
+            typeof(Func<,>),
+            typeof(Func<,,>),
+            typeof(Func<,,,>),
+            typeof(Func<,,,,>),
+            typeof(Func<,,,,,>),
+            typeof(Func<,,,,,,>),
+            typeof(Func<,,,,,,,>),
+            typeof(Func<,,,,,,,,>),
+            typeof(Func<,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,,,,,>),
+            typeof(Func<,,,,,,,,,,,,,,,>)
+        };
+
+        public bool IncludeNulls { get; set; }
+
+        public bool IncludeEmptyCollections { get; set; }
+
+        public BindingOptions BindingOptions { get; set; }
+
+        public string NullValueOutput { get; set; } = "<null>";
+
+        public string EmptyValueOutput { get; set; } = "<empty>";
+
+        public ObjectPropertySerializationHelper(bool includeNulls = false, bool includeEmptyCollections = false, BindingOptions bindingOptions = BindingOptions.Default)
+        {
+            IncludeNulls = includeNulls;
+            IncludeEmptyCollections = includeEmptyCollections;
+            BindingOptions = bindingOptions;
+        }
+
+        public IDictionary<string, string> SerializeToDictionary(object instance)
+        {
+            _ = instance.WhenNotNull(nameof(instance));
+
+            var dictionary = new Dictionary<string, string>();
+
+            if (instance != null)
+            {
+                Populate(null, instance, dictionary);
+            }
+
+            return dictionary;
+        }
+
+        public void IgnoreTypes(params Type[] types)
+        {
+            IgnoredTypes.AddRange(types);
+        }
+
+        private void Populate(string prefix, object instance, IDictionary<string, string> values)
+        {
+            switch (instance)
+            {
+                case IDictionary dictionary:
+                    AppendDictionaryAsPropertyValues(prefix, dictionary, values);
+                    break;
+
+                case IEnumerable enumerable:
+                    AppendEnumerableAsPropertyValues(prefix, enumerable, values);
+                    break;
+
+                default:
+                    AppendObjectAsPropertyValues(prefix, instance, values);
+                    break;
+            }
+        }
+
+        private void AppendDictionaryAsPropertyValues(string prefix, IDictionary dictionary, IDictionary<string, string> values)
+        {
+            var args = dictionary.GetType().GetGenericArguments();
+            var keyType = args[0];
+            var valueType = args[1];
+
+            if (IgnoreType(keyType) || IgnoreType(valueType))
+            {
+                return;
+            }
+
+            var flag = keyType.IsClass && keyType != typeof(string);
+            var num = 0;
+
+            var keyEnumerator = dictionary.Keys.GetEnumerator();
+            var valueEnumerator = dictionary.Values.GetEnumerator();
+
+            while (keyEnumerator.MoveNext())
+            {
+                valueEnumerator.MoveNext();
+
+                var namePrefix = prefix.IsNullOrEmpty()
+                    ? string.Empty
+                    : $"{prefix}.";
+
+                AppendNameValue(flag
+                        ? $"{namePrefix}{keyType.Name}`{num}"
+                        : $"{namePrefix}{keyEnumerator.Current}", valueEnumerator.Current, values
+                );
+
+                ++num;
+            }
+
+            if (!IncludeEmptyCollections || num != 0)
+            {
+                return;
+            }
+
+            AppendNameValue(prefix, EmptyValueOutput, values);
+        }
+
+        private void AppendEnumerableAsPropertyValues(string prefix, IEnumerable enumerable, IDictionary<string, string> values)
+        {
+            var args = enumerable.GetType().GetGenericArguments();
+
+            if (args.Any() && IgnoreType(args[0]))
+            {
+                return;
+            }
+
+            var num = 0;
+
+            foreach (var value in enumerable)
+            {
+                AppendNameValue($"{prefix}[{num++}]", value, values);
+            }
+
+            if (!IncludeEmptyCollections || num != 0)
+            {
+                return;
+            }
+
+            AppendNameValue(prefix, EmptyValueOutput, values);
+        }
+
+        private void AppendObjectAsPropertyValues(string prefix, object instance, IDictionary<string, string> values)
+        {
+            foreach (var propertyInfo in instance.GetType().GetPropertyInfo(BindingOptions).Where(prop => prop.CanRead))
+            {
+                var obj = propertyInfo.GetValue(instance);
+
+                if (IncludeNulls || obj != null)
+                {
+                    var name = propertyInfo.Name;
+                    
+                    if (!prefix.IsNullOrEmpty())
+                    {
+                        name = prefix + "." + name;
+                    }
+
+                    AppendNameValue(name, obj, values);
+                }
+            }
+        }
+
+        private void AppendNameValue(string name, object value, IDictionary<string, string> values)
+        {
+            if (value == null)
+            {
+                values.Add(name, NullValueOutput);
+            }
+            else
+            {
+                var type = value.GetType();
+
+                if (IgnoreType(type))
+                {
+                    return;
+                }
+
+                if (type.IsValueType || type == typeof(string))
+                {
+                    values.Add(name, $"{value}");
+                }
+                else
+                {
+                    Populate(name, value, values);
+                }
+            }
+        }
+
+        private bool IgnoreType(Type type)
+        {
+            if (IgnoredTypes.Contains(type))
+            {
+                return true;
+            }
+
+            return type.IsGenericType && IgnoredTypes.Contains(type.GetGenericTypeDefinition());
+        }
+    }
+}
