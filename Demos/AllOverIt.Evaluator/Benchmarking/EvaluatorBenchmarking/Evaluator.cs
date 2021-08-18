99cf7d7617360c61645b0ea7e745b9b7770e8e3c
Malcolm Smith <malcolm@mjfreelancing.com>
2021-08-18 23:31:12 +1000

    Benchmark stats update

--- Demos/AllOverIt.Evaluator/Benchmarking/EvaluatorBenchmarking/Evaluator.cs
+++ Demos/AllOverIt.Evaluator/Benchmarking/EvaluatorBenchmarking/Evaluator.cs
@@ -157,6 +157,9 @@ namespace EvaluatorBenchmarking
             var b = _variableRegistry.AddDelegateVariable("b", distanceB);
             var c = _variableRegistry.AddDelegateVariable("c", distanceC);
 
+            // and use this to find the area of the bound triangle using Heron's formula
+            var herons = _compiler.Compile("0.25 * sqrt((4*a^2*b^2)-(a^2+b^2-c^2)^2)", _variableRegistry).Resolver;
+
             for (var i = 0; i < iterations; i++)
             {
                 // set the co-ordinates of the 3 points
@@ -178,8 +181,6 @@ namespace EvaluatorBenchmarking
                 // distance x2,y2 to x3,y3
                 var d3 = c.Value;
 
-                // and use this to find the area of the bound triangle using Heron's formula
-                var herons = _compiler.Compile("0.25 * sqrt((4*a^2*b^2)-(a^2+b^2-c^2)^2)", _variableRegistry).Resolver;
                 var _ = herons.Invoke();
             }
         }
