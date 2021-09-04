5a45b82f94f98675f7db285797a9d2c0b1f93b71
Malcolm Smith <malcolm@mjfreelancing.com>
2021-09-05 01:41:38 +1000

    Added RaiiAsync, removed unneeded  serializables

--- Source/AllOverIt/Helpers/Raii.cs
+++ Source/AllOverIt/Helpers/Raii.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 
+#if NETSTANDARD2_1_OR_GREATER
+using System.Threading.Tasks;
+#endif
+
 namespace AllOverIt.Helpers
 {
     /// <summary>A disposable object implementing the Resource Acquisition Is Initialization idiom.</summary>
@@ -45,7 +49,7 @@ namespace AllOverIt.Helpers
     }
 
     /// <summary>
-    /// A strongly-type disposable object implementing the Resource Acquisition Is Initialization  idiom.
+    /// A strongly-type disposable object implementing the Resource Acquisition Is Initialization idiom.
     /// </summary>
     /// <typeparam name="TType">The type being initialized.</typeparam>
     public class Raii<TType> : IDisposable
@@ -53,6 +57,7 @@ namespace AllOverIt.Helpers
         private bool _disposed;
         private readonly Action<TType> _cleanUp;
 
+        /// <summary>The context provided at the time of initialization.</summary>
         public TType Context { get; private set; }
 
         /// <summary>Constructor used to provide the initialization and cleanup actions to be invoked.</summary>
@@ -66,10 +71,12 @@ namespace AllOverIt.Helpers
             _cleanUp = cleanUp ?? throw new ArgumentNullException(nameof(cleanUp));
         }
 
-        /// <summary>This is called when the instance is being disposed.</summary>
+        /// <summary>Called when the instance is being disposed, resulting in the cleanup action provided at the time of
+        /// construction being invoked.</summary>
         public void Dispose()
         {
             Dispose(true);
+            GC.SuppressFinalize(this);
         }
 
         /// <summary>
@@ -91,4 +98,50 @@ namespace AllOverIt.Helpers
             }
         }
     }
+
+#if NETSTANDARD2_1_OR_GREATER
+    /// <summary>
+    /// A strongly-type async disposable object implementing the Resource Acquisition Is Initialization idiom.
+    /// </summary>
+    /// <typeparam name="TType">The type being initialized.</typeparam>
+    public class RaiiAsync<TType> : IAsyncDisposable
+    {
+        private Func<TType, Task> _cleanUp;
+
+        /// <summary>The context provided at the time of initialization.</summary>
+        public TType Context { get; }
+
+        /// <summary>Constructor used to provide the initialization and cleanup actions to be invoked.</summary>
+        /// <param name="initialize">The initialization action to invoke at the time of initialization.</param>
+        /// <param name="cleanUp">The cleanup action to perform when the object is disposed.</param>
+        public RaiiAsync(Func<TType> initialize, Func<TType, Task> cleanUp)
+        {
+            _ = initialize ?? throw new ArgumentNullException(nameof(initialize));
+
+            Context = initialize.Invoke();
+            _cleanUp = cleanUp ?? throw new ArgumentNullException(nameof(cleanUp));
+        }
+
+        /// <summary>Called asynchronously when the instance is being disposed, resulting in the cleanup action
+        /// provided at the time of construction being invoked.</summary>
+        public async ValueTask DisposeAsync()
+        {
+            await DisposeAsyncCore();
+
+#pragma warning disable CA1816 // Dispose methods should call SuppressFinalize
+            GC.SuppressFinalize(this);
+#pragma warning restore CA1816 // Dispose methods should call SuppressFinalize
+        }
+
+        protected virtual async ValueTask DisposeAsyncCore()
+        {
+            if (_cleanUp != null)
+            {
+                await _cleanUp.Invoke(Context).ConfigureAwait(false);
+            }
+
+            _cleanUp = null;
+        }
+    }
+#endif
 }
