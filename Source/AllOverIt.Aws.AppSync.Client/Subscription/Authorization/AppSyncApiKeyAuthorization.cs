fba482cb289ab029e92272b8549be31afb2a0d8a
Malcolm Smith <malcolm@mjfreelancing.com>
2021-09-09 12:47:03 +1000

    Refactoring

--- Source/AllOverIt.Aws.AppSync.Client/Subscription/Authorization/AppSyncApiKeyAuthorization.cs
+++ Source/AllOverIt.Aws.AppSync.Client/Subscription/Authorization/AppSyncApiKeyAuthorization.cs
@@ -1,4 +1,4 @@
-﻿namespace AllOverIt.Aws.AppSync.Client.Subscription
+﻿namespace AllOverIt.Aws.AppSync.Client.Subscription.Authorization
 {
     public sealed class AppSyncApiKeyAuthorization : AppSyncAuthorizationBase
     {
