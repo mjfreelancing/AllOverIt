4fef713b5ded2e1369f81d40d20f52335c11cb68
Malcolm Smith <malcolm@mjfreelancing.com>
2021-09-21 14:23:08 +1000

    Reverted class (cannot be abstract)

--- Source/AllOverIt.Aws.AppSync.Client/Response/WebSocketGraphqlResponse.cs
+++ Source/AllOverIt.Aws.AppSync.Client/Response/WebSocketGraphqlResponse.cs
@@ -1,6 +1,6 @@
 ﻿namespace AllOverIt.Aws.AppSync.Client.Response
 {
-    public abstract class WebSocketGraphqlResponse<TPayload>
+    public class WebSocketGraphqlResponse<TPayload>
     {
         public string Type { get; init; }
         public TPayload Payload { get; init; }
