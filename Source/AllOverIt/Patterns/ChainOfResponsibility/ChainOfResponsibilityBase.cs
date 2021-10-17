cb33bde4a6dc766246f0f9bf61847ab152251197
Malcolm Smith <malcolm@mjfreelancing.com>
2021-10-17 22:36:58 +1100

    Comment updates

--- Source/AllOverIt/Patterns/ChainOfResponsibility/ChainOfResponsibilityBase.cs
+++ Source/AllOverIt/Patterns/ChainOfResponsibility/ChainOfResponsibilityBase.cs
@@ -6,20 +6,25 @@ namespace AllOverIt.Patterns.ChainOfResponsibility
     {
         private IChainOfResponsibility<TInput, TOutput> _nextHandler;
 
+        /// <inheritdoc />
         public IChainOfResponsibility<TInput, TOutput> SetNext(IChainOfResponsibility<TInput, TOutput> handler)
         {
             _nextHandler = handler.WhenNotNull(nameof(handler));
             return handler;
         }
 
-        public virtual TOutput Handle(TInput request)
+        /// <inheritdoc />
+        /// <remarks>If the current handler cannot process the request then it should call base.Handle() to give
+        /// the next handler in the chain an opportunity to process the request. To terminate the processing
+        /// at the current handler do not call the base method.</remarks>
+        public virtual TOutput Handle(TInput state)
         {
             // The last handler passed to SetNext() will have its _nextHandler un-assigned - return default
             // to indicate it's the end of the chain. A better practice would be to include a terminal handler
             // that does not call base.Handle() at the end of its processing.
             return _nextHandler == null
               ? default
-              : _nextHandler.Handle(request);
+              : _nextHandler.Handle(state);
         }
     }
 }
