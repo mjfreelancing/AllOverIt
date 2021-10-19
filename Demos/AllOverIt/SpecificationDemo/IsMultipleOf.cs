0285a8dfdf3902a208a94c738f5ef249e2b0d5b5
Malcolm Smith <malcolm@mjfreelancing.com>
2021-10-19 21:49:57 +1100

    Added specification demo

---
+++ Demos/AllOverIt/SpecificationDemo/IsMultipleOf.cs
@@ -0,0 +1,19 @@
+﻿using AllOverIt.Patterns.Specification;
+
+namespace SpecificationDemo
+{
+    internal sealed class IsMultipleOf : Specification<int>
+    {
+        private int Value { get; }
+
+        public IsMultipleOf(int value)
+        {
+            Value = value;
+        }
+
+        protected override bool DoIsSatisfiedBy(int candidate)
+        {
+            return candidate % Value == 0;
+        }
+    }
+}
---
+++ Demos/AllOverIt/SpecificationDemo/Program.cs
@@ -0,0 +1,117 @@
+﻿using AllOverIt.Extensions;
+using AllOverIt.Patterns.Specification;
+using System;
+using System.Linq;
+
+namespace SpecificationDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            #region Define specifications
+
+            var multipleOfTwo = new IsMultipleOf(2);
+            var multipleOfThree = new IsMultipleOf(3);
+            var multipleOfSeven = new IsMultipleOf(7);
+
+            var twoOrThreeSpecification = multipleOfTwo.Or(multipleOfThree);
+            var twoAndThreeSpecification = multipleOfTwo.And(multipleOfThree);
+            var complexSpecification = twoAndThreeSpecification.Or(multipleOfSeven);
+
+            #endregion
+
+            ShowIndividualResults(twoOrThreeSpecification, twoAndThreeSpecification);
+            ShowDivisbleAndTest(twoAndThreeSpecification);
+            ShowDivisbleOrTest(twoOrThreeSpecification);
+            ShowComplexTest(complexSpecification);
+            ShowNotComplexTest(complexSpecification);
+
+            Console.WriteLine("");
+            Console.WriteLine("All Over It.");
+            Console.ReadKey();
+        }
+
+        #region Show Simple Specification
+
+        private static void ShowIndividualResults(ISpecification<int> twoOrThreeSpecification, ISpecification<int> twoAndThreeSpecification)
+        {
+            // test all numbers 1..10 individually
+            foreach (var i in Enumerable.Range(1, 10))
+            {
+                Console.WriteLine($"{i} is divisible by 2 or 3 {twoOrThreeSpecification.IsSatisfiedBy(i)}");
+                Console.WriteLine($"{i} is divisible by 2 and 3 = {twoAndThreeSpecification.IsSatisfiedBy(i)}");
+                Console.WriteLine("");
+            }
+        }
+
+        #endregion
+
+        private static void ShowDivisbleAndTest(ISpecification<int> twoAndThreeSpecification)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Press any key for the next test (list only those divisible by 2 and 3)...");
+            Console.ReadKey();
+            Console.WriteLine("");
+
+            // find all matches where a value is divisible by 2 and 3
+            var twoAndThreeResults = Enumerable.Range(1, 12).WhereSatisfied(twoAndThreeSpecification);
+
+            foreach (var i in twoAndThreeResults)
+            {
+                Console.WriteLine($"{i} is divisible by 2 and 3");
+            }
+        }
+
+        private static void ShowDivisbleOrTest(ISpecification<int> twoOrThreeSpecification)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Press any key for the next test (list only those divisible by 2 or 3)...");
+            Console.ReadKey();
+            Console.WriteLine("");
+
+            // find all matches where a value is divisible by 2 or 3
+            var twoOrThreeResults = Enumerable.Range(1, 12).WhereSatisfied(twoOrThreeSpecification);
+
+            foreach (var i in twoOrThreeResults)
+            {
+                Console.WriteLine($"{i} is divisible by 2 or 3");
+            }
+        }
+
+        #region Show Complex Specification
+
+        private static void ShowComplexTest(ISpecification<int> complexSpecification)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Press any key for the next test (list only those divisible by ((2 and 3) or 7))...");
+            Console.ReadKey();
+            Console.WriteLine("");
+
+            var results = Enumerable.Range(1, 21).WhereSatisfied(complexSpecification);
+
+            foreach (var i in results)
+            {
+                Console.WriteLine($"{i} is divisible by ((2 and 3) or 7)");
+            }
+        }
+
+        #endregion
+
+        private static void ShowNotComplexTest(ISpecification<int> complexSpecification)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Press any key for the next test (list only those NOT divisible by ((2 and 3) or 7))...");
+            Console.ReadKey();
+            Console.WriteLine("");
+
+            var notSpecification = complexSpecification.Not();
+            var results = Enumerable.Range(1, 21).WhereSatisfied(notSpecification);
+
+            foreach (var i in results)
+            {
+                Console.WriteLine($"{i} is NOT divisible by ((2 and 3) or 7)");
+            }
+        }
+    }
+}
