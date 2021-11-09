69e999ac62f170d6b8e039bebc0b2e39d0879054
Malcolm Smith <malcolm@mjfreelancing.com>
2021-11-09 19:09:24 +1100

    Added comments

--- Source/AllOverIt.EntityFrameworkCore/ValueConverters/EnrichedEnumNameConverter.cs
+++ Source/AllOverIt.EntityFrameworkCore/ValueConverters/EnrichedEnumNameConverter.cs
@@ -3,9 +3,12 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AllOverIt.EntityFrameworkCore.ValueConverters
 {
-    public class EnrichedEnumNameConverter<TEnum> : ValueConverter<TEnum, string>
+    /// <summary>Defines the conversion from an <see cref="EnrichedEnum{TEnum}"/> type to a string.</summary>
+    /// <typeparam name="TEnum">The enriched enum type. This must inherit <see cref="EnrichedEnum{TEnum}"/>.</typeparam>
+    public sealed class EnrichedEnumNameConverter<TEnum> : ValueConverter<TEnum, string>
         where TEnum : EnrichedEnum<TEnum>
     {
+        /// <summary>Constructor.</summary>
         public EnrichedEnumNameConverter()
             : base(item => item.Name, value => EnrichedEnum<TEnum>.From(value))
         {
