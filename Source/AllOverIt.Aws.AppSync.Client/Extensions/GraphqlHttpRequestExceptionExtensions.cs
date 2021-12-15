21ee4ff8aeeee256a7f8ac64e97dd0129be7919b
Malcolm Smith <malcolm@mjfreelancing.com>
2021-12-15 15:26:21 +1100

    Version bump and comments added

--- Source/AllOverIt.Aws.AppSync.Client/Extensions/GraphqlHttpRequestExceptionExtensions.cs
+++ Source/AllOverIt.Aws.AppSync.Client/Extensions/GraphqlHttpRequestExceptionExtensions.cs
@@ -3,10 +3,15 @@ using System.Linq;
 
 namespace AllOverIt.Aws.AppSync.Client.Extensions
 {
+    /// <summary>Provides a variety of extension methods for <see cref="GraphqlHttpRequestException"/>.</summary>
     public static class GraphqlHttpRequestExceptionExtensions
     {
-        private static readonly string ExecutionTimeoutErrorType = "ExecutionTimeout";
+        private const string ExecutionTimeoutErrorType = "ExecutionTimeout";
 
+        /// <summary>Determines if the exception contains any errors with an error type of 'ExecutionTimeout",
+        /// as reported by AppSync.</summary>
+        /// <param name="exception">The exception containing errors.</param>
+        /// <returns>True if the exception contains any errors with an error type of 'ExecutionTimeout", otherwise false.</returns>
         public static bool HasExecutionTimeoutError(this GraphqlHttpRequestException exception)
         {
             return exception.Errors.Any(error => error.ErrorType == ExecutionTimeoutErrorType);
