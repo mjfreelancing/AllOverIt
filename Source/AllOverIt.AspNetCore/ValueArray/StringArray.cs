a812043e924c306e6c1b749ce1e63dbfe0fa974e
Malcolm Smith <malcolm@mjfreelancing.com>
2022-01-15 12:20:34 +1100

    Moved ValueArray related classes

---
+++ Source/AllOverIt.AspNetCore/ValueArray/StringArray.cs
@@ -0,0 +1,11 @@
+﻿using AllOverIt.AspNetCore.ModelBinders;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AllOverIt.AspNetCore.ValueArray
+{
+    // Only applicable for models used to bind from a query string
+    [ModelBinder(typeof(ValueArrayModelBinder<StringArray, string>))]
+    public sealed record StringArray : ValueArray<string>
+    {
+    }
+}
