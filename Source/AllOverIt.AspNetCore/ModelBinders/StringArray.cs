eb23bc7762b4081e03641e127105b0331a051143
Malcolm Smith <malcolm@mjfreelancing.com>
2022-01-15 12:15:07 +1100

    Add initial model binders for array values in a QueryString

---
+++ Source/AllOverIt.AspNetCore/ModelBinders/StringArray.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AllOverIt.AspNetCore.ModelBinders
+{
+    // Only applicable for models used to bind from a query string
+    [ModelBinder(typeof(ValuesArrayModelBinder<StringArray, string>))]
+    public sealed record StringArray : ValuesArray<string>
+    {
+    }
+}
---
+++ Source/AllOverIt.AspNetCore/ModelBinders/ValuesArray.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace AllOverIt.AspNetCore.ModelBinders
+{
+    // Only applicable for models used to bind from a query string
+    public abstract record ValuesArray<TType>
+    {
+        public IReadOnlyCollection<TType> Values { get; init; }
+    }
+}
