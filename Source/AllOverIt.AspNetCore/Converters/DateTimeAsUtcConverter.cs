e2fc1d14bcb08f66831066a75d8731f0d0ab550e
Malcolm Smith <malcolm@mjfreelancing.com>
2022-01-15 12:20:46 +1100

    Added DateTimeAsUtcConverter

---
+++ Source/AllOverIt.AspNetCore/Converters/DateTimeAsUtcConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AllOverIt.AspNetCore.Converters
+{
+    public sealed class DateTimeAsUtcConverter : JsonConverter<DateTime>
+    {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var value = reader.GetDateTime();
+            return DateTime.SpecifyKind(value, DateTimeKind.Utc);
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            var utcDateTime = DateTime.SpecifyKind(value, DateTimeKind.Utc);
+            writer.WriteStringValue(utcDateTime);
+        }
+    }
+}
