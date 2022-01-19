8f2c0d5635d08ea991d907b775a08ce36d4a9374
Malcolm Smith <malcolm@mjfreelancing.com>
2022-01-19 21:52:22 +1100

    Initial code shuffling

---
+++ Source/AllOverIt.Serialization.NewtonsoftJson/Converters/DateTimeAsUtcConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace AllOverIt.Serialization.NewtonsoftJson.Converters
+{
+    /// <summary>A JsonConverter that treats <see cref="DateTime"/> values as kind <see cref="DateTimeKind.Utc"/>.</summary>
+    /// <remarks>The converter does not perform any DateTime conversions; it only sets the kind so it is treated as if it is a UTC DateTime.</remarks>
+    public sealed class DateTimeAsUtcConverter : JsonConverter
+    {
+        /// <summary>Returns true if the value to be converted is a <see cref="DateTime"/>.</summary>
+        /// <param name="objectType">The object type</param>
+        /// <returns>True if the value to be converted is a <see cref="DateTime"/>.</returns>
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(DateTime) == objectType;
+        }
+
+        /// <inheritdoc />
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var dateTime = serializer.Deserialize<DateTime>(reader);
+            return DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+        }
+
+        /// <inheritdoc />
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var utcDateTime = DateTime.SpecifyKind((DateTime)value, DateTimeKind.Utc);
+            writer.WriteValue(utcDateTime);
+        }
+    }
+}
---
+++ Source/AllOverIt.Serialization.NewtonsoftJson/Converters/EnrichedEnumJsonConverter.cs
@@ -0,0 +1,37 @@
+﻿using AllOverIt.Patterns.Enumeration;
+using Newtonsoft.Json;
+using System;
+
+namespace AllOverIt.Serialization.NewtonsoftJson.Converters
+{
+    /// <summary>Converts JSON to and from an <see cref="EnrichedEnum{TEnum}"/> type.</summary>
+    /// <typeparam name="TEnum">The concrete <see cref="EnrichedEnum{TEnum}"/> type.</typeparam>
+    public class EnrichedEnumJsonConverter<TEnum> : JsonConverter
+        where TEnum : EnrichedEnum<TEnum>
+    {
+        /// <summary>Returns true if the value to be converted is a string.</summary>
+        /// <param name="objectType">The object type</param>
+        /// <returns>True if the value to be converted is a string.</returns>
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(string);
+        }
+
+        /// <inheritdoc />
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var strValue = reader.ReadAsString();
+
+            return strValue == null
+                ? null
+                : EnrichedEnum<TEnum>.From(strValue);
+        }
+
+        /// <inheritdoc />
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var enumValue = ((EnrichedEnum<TEnum>) value).Name;
+            writer.WriteValue(enumValue);
+        }
+    }
+}
