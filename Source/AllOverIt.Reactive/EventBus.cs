ba686046c0487608bb55c3a0cd686c8c917f2847
Malcolm Smith <malcolm@mjfreelancing.com>
2022-02-27 10:59:50 +1100

    Comment updates

--- Source/AllOverIt.Reactive/EventBus.cs
+++ Source/AllOverIt.Reactive/EventBus.cs
@@ -2,31 +2,36 @@
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 
-namespace AllOverIt.ReactiveUI
+namespace AllOverIt.Reactive
 {
-    internal sealed class EventBus : IEventBus
+    /// <summary>Implements a subscribable event aggregator / bus that consumers can subscribe to for notification of various event types.</summary>
+    public sealed class EventBus : IEventBus
     {
         private Subject<object> _subject = new();
 
+        /// <inheritdoc />
         public void Publish<TEvent>() where TEvent : new()
         {
             _subject.OnNext(new TEvent());
         }
 
-        public void Publish<TEvent>(TEvent sampleEvent)
+        /// <inheritdoc />
+        public void Publish<TEvent>(TEvent @event)
         {
-            _subject.OnNext(sampleEvent);
+            _subject.OnNext(@event);
         }
 
+        /// <inheritdoc />
+        public IObservable<TEvent> GetEvent<TEvent>()
+        {
+            return _subject.OfType<TEvent>().AsObservable();
+        }
+
+        /// <summary>Disposes of the observable sequence used for notifying observers of various events.</summary>
         public void Dispose()
         {
             _subject?.Dispose();
             _subject = null;
-        }
-
-        public IObservable<TEvent> GetEvent<TEvent>()
-        {
-            return _subject.OfType<TEvent>().AsObservable();
         }
     }
 }
--- Source/AllOverIt.Reactive/Extensions/ObservableExtensions.cs
+++ Source/AllOverIt.Reactive/Extensions/ObservableExtensions.cs
@@ -1,19 +1,39 @@
 ﻿using System;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
+using AllOverIt.Assertion;
 
 namespace AllOverIt.Reactive.Extensions
 {
+    /// <summary>Provides extension methods for <see cref="IObservable{T}"/>.</summary>
     public static class ObservableExtensions
     {
+        /// <summary>Subscribes to an observable and waits for a specified condition to be satisfied before returning a result.</summary>
+        /// <typeparam name="TResult">The type being observed and returned from the observable.</typeparam>
+        /// <param name="observable">The observable to wait for.</param>
+        /// <param name="predicate">The condition to be satisfied.</param>
+        /// <param name="action">An optional action to apply to the result before returning it.</param>
+        /// <returns>The result of the observable.</returns>
         public static Task<TResult> WaitUntilAsync<TResult>(
             this IObservable<TResult> observable,
             Func<TResult, bool> predicate,
-            Func<TResult, Task<TResult>> action)
+            Func<TResult, Task<TResult>> action = default)
         {
+            _ = observable.WhenNotNull(nameof(observable));
+            _ = predicate.WhenNotNull(nameof(predicate));
+
+            action ??= Task.FromResult;
+
             return observable.WaitUntilAsync<TResult, TResult>(predicate, action);
         }
 
+        /// <summary>Subscribes to an observable and waits for a specified condition to be satisfied before returning a projected result.</summary>
+        /// <typeparam name="TType">The type being observed.</typeparam>
+        /// <typeparam name="TResult">The projected result type of the observable.</typeparam>
+        /// <param name="observable">The observable to wait for.</param>
+        /// <param name="predicate">The condition to be satisfied.</param>
+        /// <param name="action">An action to apply to the observable result so it can be projected to another result type.</param>
+        /// <returns>The projected result of the observable.</returns>
         public static async Task<TResult> WaitUntilAsync<TType, TResult>(
             this IObservable<TType> observable,
             Func<TType, bool> predicate,
