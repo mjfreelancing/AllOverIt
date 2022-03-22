0f54d851dc4efddacf6a943285b9f0b8f33e74ea
Malcolm Smith <malcolm@mjfreelancing.com>
2022-03-22 20:07:37 +1100

    Added TextStreamer

---
+++ Demos/AllOverIt/Io/StringStreamDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using AllOverIt.Io;
+
+namespace StringStreamDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Construct the streamer with an initial string
+            var stream = new TextStreamer($"{DateTime.Now:dd-MMM-yyyy}");
+
+            // Write the remaining content using the underlying writer
+            var writer = stream.GetWriter();
+
+            writer.WriteLine();
+
+            writer.Write("Bool=");
+            writer.Write(true);
+            writer.WriteLine();
+
+            writer.WriteLine("Value=100");
+
+            var sb = new StringBuilder();
+            sb.Append(1);
+            sb.Append(2);
+            sb.Append(3);
+
+            await writer.WriteLineAsync(sb);
+
+            var output = stream.ToString();
+            Console.WriteLine("Streamed:");
+            Console.WriteLine($"{output}");
+            Console.WriteLine();
+
+            Console.WriteLine("All Over It.");
+            Console.ReadKey();
+        }
+    }
+}
