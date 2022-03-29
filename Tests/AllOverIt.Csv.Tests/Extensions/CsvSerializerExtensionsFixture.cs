43af1f5af6ee3761d4f66697dc547daf55ed74c2
Malcolm Smith <malcolm@mjfreelancing.com>
2022-03-29 22:50:11 +1100

    Added more tests

--- Tests/AllOverIt.Csv.Tests/Extensions/CsvSerializerExtensionsFixture.cs
+++ Tests/AllOverIt.Csv.Tests/Extensions/CsvSerializerExtensionsFixture.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using AllOverIt.Csv.Extensions;
+﻿using AllOverIt.Csv.Extensions;
 using AllOverIt.Extensions;
 using AllOverIt.Fixture;
 using AllOverIt.Fixture.Extensions;
 using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AllOverIt.Csv.Exceptions;
 using Xunit;
 
 namespace AllOverIt.Csv.Tests.Extensions
@@ -22,10 +22,12 @@ namespace AllOverIt.Csv.Tests.Extensions
             {
                 public string Name { get; set; }
                 public int Value { get; set; }
+                public bool Active { get; set; }
             }
 
             public IDictionary<string, double> KeyValues { get; set; }
             public ChildDummy Child { get; set; }
+            public IReadOnlyCollection<ChildDummy> Children { get; set; }
         }
 
         private readonly CsvSerializer<SampleDataDummy> _serializer = new();
@@ -42,7 +44,8 @@ namespace AllOverIt.Csv.Tests.Extensions
                     KeyValues = CreateMany<KeyValuePair<string, double>>()
                         .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
 
-                    Child = Create<SampleDataDummy.ChildDummy>()
+                    Child = Create<SampleDataDummy.ChildDummy>(),
+                    Children = CreateMany<SampleDataDummy.ChildDummy>()
                 },
                 new SampleDataDummy
                 {
@@ -50,14 +53,16 @@ namespace AllOverIt.Csv.Tests.Extensions
                         .Select(key => new KeyValuePair<string, double>(key, Create<double>()))
                         .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
 
-                    Child = Create<SampleDataDummy.ChildDummy>()
+                    Child = Create<SampleDataDummy.ChildDummy>(),
+                    Children = CreateMany<SampleDataDummy.ChildDummy>()
                 },
                 new SampleDataDummy
                 {
                     KeyValues = CreateMany<KeyValuePair<string, double>>()
                         .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
 
-                    Child = Create<SampleDataDummy.ChildDummy>()
+                    Child = Create<SampleDataDummy.ChildDummy>(),
+                    Children = CreateMany<SampleDataDummy.ChildDummy>()
                 },
                 new SampleDataDummy
                 {
@@ -65,14 +70,16 @@ namespace AllOverIt.Csv.Tests.Extensions
                         .Select(key => new KeyValuePair<string, double>(key, Create<double>()))
                         .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
 
-                    Child = Create<SampleDataDummy.ChildDummy>()
+                    Child = Create<SampleDataDummy.ChildDummy>(),
+                    Children = CreateMany<SampleDataDummy.ChildDummy>()
                 },
                 new SampleDataDummy
                 {
                     KeyValues = CreateMany<KeyValuePair<string, double>>()
                         .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
 
-                    Child = Create<SampleDataDummy.ChildDummy>()
+                    Child = Create<SampleDataDummy.ChildDummy>(),
+                    Children = CreateMany<SampleDataDummy.ChildDummy>()
                 },
             };
         }
@@ -239,67 +246,233 @@ namespace AllOverIt.Csv.Tests.Extensions
             }
         }
 
-        //public class AddDynamicFields_Field_FieldId : CsvSerializerExtensionsFixture
-        //{
-        //    [Fact]
-        //    public void Should_Throw_When_Serializer_Null()
-        //    {
-        //        Invoking(() =>
-        //            {
+        public class AddDynamicFields_Field_FieldId : CsvSerializerExtensionsFixture
+        {
+                [Fact]
+            public void Should_Throw_When_Serializer_Null()
+            {
+                Invoking(() =>
+                    {
+                        CsvSerializerExtensions.AddDynamicFields<SampleDataDummy, IReadOnlyCollection<SampleDataDummy.ChildDummy>, int>(
+                            null,
+                            _sampleData,
+                            data => data.Children,
+                            field => new FieldIdentifier<int>[]{},
+                            (field, identifier) => new object[]{}
+                        );
+                    })
+                    .Should()
+                    .Throw<ArgumentNullException>()
+                    .WithNamedMessageWhenNull("serializer");
+            }
 
-        //            })
-        //            .Should()
-        //            .Throw<ArgumentNullException>()
-        //            .WithNamedMessageWhenNull("");
-        //    }
+            [Fact]
+            public void Should_Throw_When_Data_Null()
+            {
+                Invoking(() =>
+                    {
+                        CsvSerializerExtensions.AddDynamicFields<SampleDataDummy, IReadOnlyCollection<SampleDataDummy.ChildDummy>, int>(
+                            _serializer,
+                            null,
+                            data => data.Children,
+                            field => new FieldIdentifier<int>[] { },
+                            (field, identifier) => new object[] { }
+                        );
+                    })
+                    .Should()
+                    .Throw<ArgumentNullException>()
+                    .WithNamedMessageWhenNull("data");
+            }
 
-        //    [Fact]
-        //    public void Should_Throw_When_Data_Null()
-        //    {
-        //        Invoking(() =>
-        //            {
+            [Fact]
+            public void Should_Throw_When_Field_Selector_Null()
+            {
+                Invoking(() =>
+                    {
+                        CsvSerializerExtensions.AddDynamicFields<SampleDataDummy, IReadOnlyCollection<SampleDataDummy.ChildDummy>, int>(
+                            _serializer,
+                            _sampleData,
+                            null,
+                            field => new FieldIdentifier<int>[] { },
+                            (field, identifier) => new object[] { }
+                        );
+                    })
+                    .Should()
+                    .Throw<ArgumentNullException>()
+                    .WithNamedMessageWhenNull("fieldSelector");
+            }
 
-        //            })
-        //            .Should()
-        //            .Throw<ArgumentNullException>()
-        //            .WithNamedMessageWhenNull("");
-        //    }
+            [Fact]
+            public void Should_Throw_When_Field_Identifiers_Null()
+            {
+                Invoking(() =>
+                    {
+                        CsvSerializerExtensions.AddDynamicFields<SampleDataDummy, IReadOnlyCollection<SampleDataDummy.ChildDummy>, int>(
+                            _serializer,
+                            _sampleData,
+                            data => data.Children,
+                            null,
+                            (field, identifier) => new object[] { }
+                        );
+                    })
+                    .Should()
+                    .Throw<ArgumentNullException>()
+                    .WithNamedMessageWhenNull("fieldIdentifiers");
+            }
 
-        //    [Fact]
-        //    public void Should_Throw_When_Field_Selector_Null()
-        //    {
-        //        Invoking(() =>
-        //            {
+            [Fact]
+            public void Should_Throw_When_Values_Resolver_Null()
+            {
+                Invoking(() =>
+                    {
+                        CsvSerializerExtensions.AddDynamicFields<SampleDataDummy, IReadOnlyCollection<SampleDataDummy.ChildDummy>, int>(
+                            _serializer,
+                            _sampleData,
+                            data => data.Children,
+                            field => new FieldIdentifier<int>[] { },
+                            null
+                        );
+                    })
+                    .Should()
+                    .Throw<ArgumentNullException>()
+                    .WithNamedMessageWhenNull("valuesResolver");
+            }
 
-        //            })
-        //            .Should()
-        //            .Throw<ArgumentNullException>()
-        //            .WithNamedMessageWhenNull("");
-        //    }
+            [Fact]
+            public async Task Should_Export_Dynamic_Columns_From_Object()
+            {
+                CsvSerializerExtensions.AddDynamicFields<SampleDataDummy, IReadOnlyCollection<SampleDataDummy.ChildDummy>, KeyValuePair<string, int>>(
+                    _serializer,
+                    _sampleData,
+                    data => data.Children,
+                    field => 
+                    {
+                        return field
+                            .Select(child =>
+                            {
+                                return new FieldIdentifier<KeyValuePair<string, int>>
+                                {
+                                    // Used as a lookup key
+                                    Id = new KeyValuePair<string, int>(child.Name, child.Value),
 
-        //    [Fact]
-        //    public void Should_Throw_When_Field_Identifiers_Null()
-        //    {
-        //        Invoking(() =>
-        //            {
+                                    // One or more columns to output
+                                    Names = new[]
+                                    {
+                                        $"{child.Name}-{child.Value}"
+                                    }
+                                };
+                            });
+                    },
+                    (item, headerId) =>
+                    {
+                        var (name, value) = headerId.Id;
 
-        //            })
-        //            .Should()
-        //            .Throw<ArgumentNullException>()
-        //            .WithNamedMessageWhenNull("");
-        //    }
+                        var child = item.SingleOrDefault(data => data.Name == name && data.Value == value);
 
-        //    [Fact]
-        //    public void Should_Throw_When_Values_Resolver_Null()
-        //    {
-        //        Invoking(() =>
-        //            {
+                        return child == null
+                            ? null
+                            : new object[]
+                            {
+                                child.Active
+                            };
+                    }
+                );
 
-        //            })
-        //            .Should()
-        //            .Throw<ArgumentNullException>()
-        //            .WithNamedMessageWhenNull("");
-        //    }
-        //}
+                // Get the actual output
+                string actual;
+
+                using (var writer = new StringWriter())
+                {
+                    await _serializer.SerializeAsync(writer, _sampleData);
+                    actual = writer.ToString();
+                }
+
+                // Build the expected output
+                var sb = new StringBuilder();
+
+                var headerNames = _sampleData
+                    .SelectMany(item => item.Children)
+                    .SelectAsReadOnlyCollection(item => $"{item.Name}-{item.Value}");
+
+                sb.AppendLine(string.Join(",", headerNames));
+
+                foreach (var data in _sampleData)
+                {
+                    var cellValues = new List<string>();
+
+                    foreach (var headerName in headerNames)
+                    {
+                        var child = data.Children.SingleOrDefault(item => headerName == $"{item.Name}-{item.Value}");
+
+                        var cellValue = child != null
+                            ? $"{child.Active}"
+                            : string.Empty;
+
+                        cellValues.Add(cellValue);
+                    }
+
+                    sb.AppendLine(string.Join(",", cellValues));
+                }
+
+                var expected = sb.ToString();
+
+                expected.Should().Be(actual);
+            }
+
+            [Fact]
+            public async Task Should_Throw_When_Column_Counts_Mismatch()
+            {
+                CsvSerializerExtensions.AddDynamicFields<SampleDataDummy, IReadOnlyCollection<SampleDataDummy.ChildDummy>, KeyValuePair<string, int>>(
+                    _serializer,
+                    _sampleData,
+                    data => data.Children,
+                    field =>
+                    {
+                        return field
+                            .Select(child =>
+                            {
+                                return new FieldIdentifier<KeyValuePair<string, int>>
+                                {
+                                    // Used as a lookup key
+                                    Id = new KeyValuePair<string, int>(child.Name, child.Value),
+
+                                    // One or more columns to output
+                                    Names = new[]
+                                    {
+                                        // Returning two columns from here but only a single value below
+                                        child.Name,
+                                        $"{child.Value}"
+                                    }
+                                };
+                            });
+                    },
+                    (item, headerId) =>
+                    {
+                        var (name, value) = headerId.Id;
+
+                        var child = item.SingleOrDefault(data => data.Name == name && data.Value == value);
+
+                        return child == null
+                            ? null
+                            : new object[]
+                            {
+                                // Only one value here, but two columns are defined above
+                                child.Active
+                            };
+                    }
+                );
+
+                await Invoking(async () =>
+                    {
+                        using (var writer = new StringWriter())
+                        {
+                            await _serializer.SerializeAsync(writer, _sampleData);
+                        }
+                    })
+                    .Should()
+                    .ThrowAsync<CsvExportException>()
+                    .WithMessage("Column count mismatch. Expected 2, found 1.");
+            }
+        }
     }
 }
