ab5ed83230bfeb9e50c84512aacb21cfb7e0f3fa
Malcolm Smith <malcolm@mjfreelancing.com>
2022-04-19 10:52:29 +1000

    Cleanup

--- Source/AllOverIt.AspNetCore/Extensions/MvcOptionsExtensions.cs
+++ Source/AllOverIt.AspNetCore/Extensions/MvcOptionsExtensions.cs
@@ -6,7 +6,7 @@ namespace AllOverIt.AspNetCore.Extensions
     /// <summary>Provides a variety of extension methods for <see cref="MvcOptions"/> types.</summary>
     public static class MvcOptionsExtensions
     {
-        /// <summary>Adds support for <see cref="EnrichedEnum{T}" model binding/>.</summary>
+        /// <summary>Adds support for <see cref="Patterns.Enumeration.EnrichedEnum{TEnum}" /> model binding.</summary>
         /// <param name="options">The <see cref="MvcOptions"/> instance.</param>
         /// <returns>The original <see cref="MvcOptions"/> instance provided to cater for a fluent syntax.</returns>
         public static MvcOptions AddEnrichedEnumModelBinder(this MvcOptions options)
