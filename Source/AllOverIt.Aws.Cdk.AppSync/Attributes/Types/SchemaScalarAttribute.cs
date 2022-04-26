08b74d2a62746a4d4482e8ecf8286085e0533e60
Malcolm Smith <malcolm@mjfreelancing.com>
2022-04-26 10:36:56 +1000

    Updated SchemaScalarAttribute to support namespace-based name creation

--- Source/AllOverIt.Aws.Cdk.AppSync/Attributes/Types/SchemaScalarAttribute.cs
+++ Source/AllOverIt.Aws.Cdk.AppSync/Attributes/Types/SchemaScalarAttribute.cs
@@ -3,10 +3,26 @@
     /// <summary>Apply to a class that describes a schema 'scalar' type.</summary>
     public sealed class SchemaScalarAttribute : SchemaTypeBaseAttribute
     {
+        /// <summary>When not null or empty, identifies the portion of the scalar's namespace to exclude from the
+        /// generated name.</summary>
+        public SchemaScalarAttribute()
+            : base(GraphqlSchemaType.Input)
+        {
+        }
+
         /// <summary>Constructor.</summary>
-        /// <param name="name">The name of the schema 'scalar' type to apply to the Graphql schema.</param>
+        /// <param name="name">The Graphql schema 'scalar' name.</param>
         public SchemaScalarAttribute(string name)
-            : base(name, GraphqlSchemaType.Scalar)
+            : base(name, GraphqlSchemaType.Input)
+        {
+        }
+
+        /// <summary>Constructor. The Graphql schema scalar's name will be based on the namespace of the class/interface the
+        /// attribute is associated with and an optionally specified name.</summary>
+        /// <param name="excludeNamespacePrefix">The portion of the scalar's namespace to exclude from the generated name.</param>
+        /// <param name="name">An optional name to append to the namespace. This can be null or an empty string if not required.</param>
+        public SchemaScalarAttribute(string excludeNamespacePrefix, string name)
+            : base(excludeNamespacePrefix, name, GraphqlSchemaType.Input)
         {
         }
     }
