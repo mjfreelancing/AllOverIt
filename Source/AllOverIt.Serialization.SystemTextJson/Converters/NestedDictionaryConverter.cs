3efd18c5a6042b61888f2b7dbe6e5b87181ac206
Malcolm Smith <malcolm@mjfreelancing.com>
2022-04-30 22:46:15 +1000

    Improvements to JsonHelper and NestedDictionaryConverter to better handle case-insenstive property querying

--- Source/AllOverIt.Serialization.SystemTextJson/Converters/NestedDictionaryConverter.cs
+++ Source/AllOverIt.Serialization.SystemTextJson/Converters/NestedDictionaryConverter.cs
@@ -8,22 +8,9 @@ namespace AllOverIt.Serialization.SystemTextJson.Converters
 {
     /// <summary>Implements a JSON Converter that converts to and from a Dictionary&lt;string, object>. All object and array
     /// properties are also converted to and from a Dictionary&lt;string, object>.</summary>
-    public sealed class NestedDictionaryConverter : JsonConverter<Dictionary<string, object>>
+    internal sealed class NestedDictionaryConverter : JsonConverter<Dictionary<string, object>>
     {
         private static readonly Type DictionaryType = typeof(Dictionary<string, object>);
-
-        private readonly NestedDictionaryConverterOptions _options;
-
-        /// <summary>Constructor.</summary>
-        /// <param name="options">Options that control how the converter behaves. If null is provided then a default
-        /// set of <see cref="NestedDictionaryConverterOptions"/> options will be used.</param>
-        public NestedDictionaryConverter(NestedDictionaryConverterOptions options = default)
-        {
-            _options = options ?? new NestedDictionaryConverterOptions
-            {
-                StrictPropertyNames = false
-            };
-        }
 
         /// <inheritdoc />
         public override Dictionary<string, object> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
@@ -33,7 +20,7 @@ namespace AllOverIt.Serialization.SystemTextJson.Converters
                 throw CreateReadJsonSerializationException(reader.TokenType);
             }
 
-            var dictionary = new Dictionary<string, object>();
+            var dictionary = new Dictionary<string, object>(StringComparer.InvariantCultureIgnoreCase);
 
             while (reader.Read())
             {
@@ -43,8 +30,7 @@ namespace AllOverIt.Serialization.SystemTextJson.Converters
                         break;
 
                     case JsonTokenType.PropertyName:
-                        var name = reader.GetString();
-                        var propertyName = GetPropertyName(name);
+                        var propertyName = reader.GetString();
 
                         if (propertyName.IsNullOrEmpty() || !reader.Read())
                         {
@@ -108,8 +94,7 @@ namespace AllOverIt.Serialization.SystemTextJson.Converters
         {
             if (key != null)
             {
-                var propertyName = GetPropertyName(key);
-                writer.WritePropertyName(propertyName);
+                writer.WritePropertyName(key);
             }
 
             switch (objectValue)
@@ -172,20 +157,6 @@ namespace AllOverIt.Serialization.SystemTextJson.Converters
                     writer.WriteNullValue();
                     break;
             }
-        }
-
-        private string GetPropertyName(string propertyName)
-        {
-            if (_options.StrictPropertyNames || propertyName.IsNullOrEmpty() || char.IsLower(propertyName[0]))
-            {
-                return propertyName;
-            }
-
-#if NETSTANDARD2_0
-            return $"{char.ToLower(propertyName[0])}{propertyName.Substring(1)}";
-#else
-            return string.Concat(propertyName[..1].ToLower(), propertyName[1..]);
-#endif
         }
 
         private static Exception CreateReadJsonSerializationException(JsonTokenType? tokenType = default)
