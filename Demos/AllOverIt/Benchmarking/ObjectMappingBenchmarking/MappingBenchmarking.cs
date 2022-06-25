99b0ecec3645336980422f7e2d71520d7623fe1d
Malcolm Smith <malcolm@mjfreelancing.com>
2022-06-25 21:58:46 +1000

    Disable two runtimes in benchmark

--- Demos/AllOverIt/Benchmarking/ObjectMappingBenchmarking/MappingBenchmarking.cs
+++ Demos/AllOverIt/Benchmarking/ObjectMappingBenchmarking/MappingBenchmarking.cs
@@ -49,8 +49,8 @@ namespace ObjectMappingBenchmarking
     */
 
     [MemoryDiagnoser(false)]
-    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
-    [SimpleJob(RuntimeMoniker.Net50)]
+    //[SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    //[SimpleJob(RuntimeMoniker.Net50)]
     [SimpleJob(RuntimeMoniker.Net60)]
     public class MappingTests
     {
