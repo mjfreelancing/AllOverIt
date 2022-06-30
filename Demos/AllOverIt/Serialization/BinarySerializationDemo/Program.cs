a154d156ffa1551ac5fbda93a636201e7634bda9
Malcolm Smith <malcolm@mjfreelancing.com>
2022-06-30 14:26:26 +1000

    Demo WIP

--- Demos/AllOverIt/Serialization/BinarySerializationDemo/Program.cs
+++ Demos/AllOverIt/Serialization/BinarySerializationDemo/Program.cs
@@ -11,17 +11,94 @@ using System.Text.Json.Serialization;
 // TODO: Compare stream sizes of JSON, Custom Writer, Reflection based writer
 // TODO: Benchmark the Custom and Reflection based writers
 
+UseCustomReadersAndWriters();
+Console.WriteLine();
+UseReflectionReadersAndWriters();
 
-var classroom = new Classroom
+Console.WriteLine();
+Console.WriteLine("All Over It.");
+Console.ReadKey();
+
+static void UseReflectionReadersAndWriters()
 {
-    RoomId = Guid.NewGuid(),
-    Teacher = new Teacher
+}
+
+static void UseCustomReadersAndWriters()
+{
+    var classroom = CreateClassroom();
+    byte[] serializedBytes;
+
+    using (var stream = new MemoryStream())
     {
-        FirstName = "Roger",
-        LastName = "Rabbit",
-        Gender = Gender.Male
-    },
-    Students = new List<Student>
+        using (var writer = new EnrichedBinaryWriter(stream, Encoding.UTF8, true))
+        {
+            // Using writers will result in a larger stream because type information is stored
+            // for each user-defined type. A pure reflection based approach will provide a smaller
+            // stream but at the expense of reduced performance when deserializing.
+            writer.Writers.Add(new StudentWriter());
+            writer.Writers.Add(new TeacherWriter());
+            writer.Writers.Add(new ClassroomWriter());
+
+            writer.WriteObject(classroom);
+        }
+
+        serializedBytes = stream.ToArray();
+    }
+
+    var serializedString = Convert.ToBase64String(serializedBytes);
+
+    Console.WriteLine("Serialized");
+    Console.WriteLine("==========");
+    Console.WriteLine(serializedString);
+    Console.WriteLine();
+    Console.WriteLine($"  => {serializedBytes.Length} bytes");
+    Console.WriteLine();
+
+    Classroom deserializedClassroom = default;
+    var deserializedBytes = Convert.FromBase64String(serializedString);
+
+    using (var stream = new MemoryStream(deserializedBytes))
+    {
+        using (var reader = new EnrichedBinaryReader(stream, Encoding.UTF8, true))
+        {
+            reader.Readers.Add(new StudentReader());
+            reader.Readers.Add(new TeacherReader());
+            reader.Readers.Add(new ClassroomReader());
+
+            deserializedClassroom = (Classroom) reader.ReadObject();
+        }
+    }
+
+    // Serialize the deserialized classroom...
+    var serializerOptions = new JsonSerializerOptions
+    {
+        WriteIndented = true,
+        Converters = { new JsonStringEnumConverter() }
+    };
+
+    var compactClassroomString = JsonSerializer.Serialize(deserializedClassroom);
+    var indentedClassroomString = JsonSerializer.Serialize(deserializedClassroom, serializerOptions);
+
+    Console.WriteLine("Deserialized");
+    Console.WriteLine("============");
+    Console.WriteLine(indentedClassroomString);
+    Console.WriteLine();
+    Console.WriteLine($"  => {compactClassroomString.Length} bytes");
+    Console.WriteLine();
+}
+
+static Classroom CreateClassroom()
+{
+    return new Classroom
+    {
+        RoomId = Guid.NewGuid(),
+        Teacher = new Teacher
+        {
+            FirstName = "Roger",
+            LastName = "Rabbit",
+            Gender = Gender.Male
+        },
+        Students = new List<Student>
     {
         new Student
         {
@@ -43,71 +120,35 @@ var classroom = new Classroom
             Age = 13
         }
     }
-};
-
-
-
-byte[] bytes;
-
-using (var stream = new MemoryStream())
-{
-    using (var writer = new EnrichedBinaryWriter(stream, Encoding.UTF8, true))
-    {
-        // Using writers will result in a larger stream because type information is stored
-        // for each user-defined type. A pure reflection based approach will provide a smaller
-        // stream but at the expense of reduced performance when deserializing.
-        writer.Writers.Add(new StudentWriter());
-        writer.Writers.Add(new TeacherWriter());
-        writer.Writers.Add(new ClassroomWriter());
-
-        writer.WriteObject(classroom);
-    }
-
-    bytes = stream.ToArray();
+    };
 }
 
-var serialized = Convert.ToBase64String(bytes);
 
-Console.WriteLine("Serialized");
-Console.WriteLine("==========");
-Console.WriteLine(serialized);
-Console.WriteLine();
-Console.WriteLine($"  => {bytes.Length} bytes");
-Console.WriteLine();
 
-Classroom deserializedClassroom = default;
-var deserializedBytes = Convert.FromBase64String(serialized);
 
-using (var stream = new MemoryStream(deserializedBytes))
+
+public interface IObjectVisitor
 {
-    using (var reader = new EnrichedBinaryReader(stream, Encoding.UTF8, true))
-    {
-        reader.Readers.Add(new StudentReader());
-        reader.Readers.Add(new TeacherReader());
-        reader.Readers.Add(new ClassroomReader());
+    void Visit(object @object);
+}
 
-        deserializedClassroom = (Classroom)reader.ReadObject();
+
+public sealed class ObjectVisitor : IObjectVisitor
+{
+    public void Visit(object @object)
+    {
+
     }
 }
 
-// Serialize the deserialized classroom...
-var serializerOptions = new JsonSerializerOptions
+
+public sealed class ObjectBinaryWriter
 {
-    WriteIndented = true,
-    Converters = { new JsonStringEnumConverter() }
-};
+    public byte[] GetObjectBytes(object @object)
+    {
 
-var compactClassroomString = JsonSerializer.Serialize(deserializedClassroom);
-var indentedClassroomString = JsonSerializer.Serialize(deserializedClassroom, serializerOptions);
-
-Console.WriteLine("Deserialized");
-Console.WriteLine("============");
-Console.WriteLine(indentedClassroomString);
-Console.WriteLine();
-Console.WriteLine($"  => {compactClassroomString.Length} bytes");
-Console.WriteLine();
+    }
+}
 
 
-Console.WriteLine();
-Console.WriteLine("All Over It.");
-Console.ReadKey();
+
