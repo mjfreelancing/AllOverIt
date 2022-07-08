ca3a9c188e836ad5e5ef85c239acd7dabf164656
Malcolm Smith <malcolm@mjfreelancing.com>
2022-07-08 22:32:55 +1000

    XAML comment updates

--- Source/AllOverIt/Serialization/Binary/EnrichedBinaryValueReader.cs
+++ Source/AllOverIt/Serialization/Binary/EnrichedBinaryValueReader.cs
@@ -2,14 +2,21 @@
 
 namespace AllOverIt.Serialization.Binary
 {
+    /// <inheritdoc cref="IEnrichedBinaryValueReader"/>
+    /// <typeparam name="TType">The object type read by this value reader.</typeparam>
     public abstract class EnrichedBinaryValueReader<TType> : IEnrichedBinaryValueReader
     {
+        /// <inheritdoc />
         public Type Type => typeof(TType);
 
+        /// <inheritdoc />
         public abstract object ReadValue(IEnrichedBinaryReader reader);
 
+        /// <inheritdoc />
         public TValue ReadValue<TValue>(IEnrichedBinaryReader reader)
         {
+            // TODO: add a guard to check typeof(TValue) == Type
+
             return (TValue)ReadValue(reader);
         }
     }
--- Source/AllOverIt/Serialization/Binary/EnrichedBinaryValueWriter.cs
+++ Source/AllOverIt/Serialization/Binary/EnrichedBinaryValueWriter.cs
@@ -2,10 +2,14 @@
 
 namespace AllOverIt.Serialization.Binary
 {
+    /// <inheritdoc cref="IEnrichedBinaryValueWriter"/>
+    /// <typeparam name="TType">The object type written by this value writer.</typeparam>
     public abstract class EnrichedBinaryValueWriter<TType> : IEnrichedBinaryValueWriter
     {
+        /// <inheritdoc />
         public Type Type => typeof(TType);
 
+        /// <inheritdoc />
         public abstract void WriteValue(IEnrichedBinaryWriter writer, object value);
     }
 }
