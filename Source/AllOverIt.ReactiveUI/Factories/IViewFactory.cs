4d200968ac843a48b29a4706532e02fd222fd264
Malcolm Smith <malcolm@mjfreelancing.com>
2022-07-10 14:38:02 +1000

    Added ViewFactory

---
+++ Source/AllOverIt.ReactiveUI/Factories/IViewFactory.cs
@@ -0,0 +1,9 @@
+﻿using ReactiveUI;
+
+namespace AllOverIt.ReactiveUI.Factories
+{
+    public interface IViewFactory
+    {
+        IViewFor<TViewModel> CreateViewFor<TViewModel>() where TViewModel : class;
+    }
+}
---
+++ Source/AllOverIt.ReactiveUI/Factories/ViewFactory.cs
@@ -0,0 +1,22 @@
+﻿using AllOverIt.Assertion;
+using Microsoft.Extensions.DependencyInjection;
+using ReactiveUI;
+using System;
+
+namespace AllOverIt.ReactiveUI.Factories
+{
+    internal sealed class ViewFactory : IViewFactory
+    {
+        private readonly IServiceProvider _provider;
+
+        public ViewFactory(IServiceProvider provider)
+        {
+            _provider = provider.WhenNotNull(nameof(provider));
+        }
+
+        public IViewFor<TViewModel> CreateViewFor<TViewModel>() where TViewModel : class
+        {
+            return _provider.GetService<IViewFor<TViewModel>>();
+        }
+    }
+}
