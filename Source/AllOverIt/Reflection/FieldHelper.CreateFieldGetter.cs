6acf808a09ca7030c74ae56cc58b49372ed163d5
Malcolm Smith <malcolm@mjfreelancing.com>
2022-07-18 21:48:03 +1000

    Started FieldHelper tests

---
+++ Source/AllOverIt/Reflection/FieldHelper.CreateFieldGetter.cs
@@ -0,0 +1,130 @@
+﻿using AllOverIt.Assertion;
+using AllOverIt.Extensions;
+using AllOverIt.Reflection.Exceptions;
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace AllOverIt.Reflection
+{
+    public static class FieldHelper
+    {
+        private static readonly MethodInfo SetFieldMethodInfo = typeof(FieldHelper).GetMethod(
+            nameof(SetField), BindingFlags.Static | BindingFlags.NonPublic, null, Type.EmptyTypes, null);
+
+        public static Func<object, object> CreateFieldGetter(FieldInfo fieldInfo)
+        {
+            _ = fieldInfo.WhenNotNull(nameof(fieldInfo));
+
+            var itemParam = Expression.Parameter(typeof(object), "item");
+            var instanceParam = itemParam.CastOrConvertTo(fieldInfo.DeclaringType);
+
+            var instanceField = Expression.Field(instanceParam, fieldInfo);
+            var objectinstanceField = Expression.Convert(instanceField, typeof(object));
+
+            return Expression
+                .Lambda<Func<object, object>>(objectinstanceField, itemParam)
+                .Compile();
+        }
+
+        public static Func<TType, object> CreateFieldGetter<TType>(FieldInfo fieldInfo)
+        {
+            _ = fieldInfo.WhenNotNull(nameof(fieldInfo));
+
+            var instance = Expression.Parameter(typeof(TType), "item");
+
+            var field = typeof(TType) != fieldInfo.DeclaringType
+                ? Expression.Field(Expression.TypeAs(instance, fieldInfo.DeclaringType), fieldInfo)
+                : Expression.Field(instance, fieldInfo);
+
+            var convertField = Expression.TypeAs(field, typeof(object));
+
+            return Expression.Lambda<Func<TType, object>>(convertField, instance).Compile();
+        }
+
+        //public static Func<TType, object> CreateFieldGetter<TType>(string fieldName)
+        //{
+        //    _ = fieldName.WhenNotNullOrEmpty(nameof(fieldName));
+
+        //    var type = typeof(TType);
+        //    var fieldInfo = ReflectionCache.GetFieldInfo(type.GetTypeInfo(), fieldName);
+
+        //    if (fieldInfo == null)
+        //    {
+        //        throw new ReflectionException($"The field {fieldName} on type {type.GetFriendlyName()} does not exist.");
+        //    }
+
+        //    return CreateFieldGetter<TType>(fieldInfo);
+        //}
+
+        public static Action<object, object> CreateFieldSetter(FieldInfo fieldInfo)
+        {
+            _ = fieldInfo.WhenNotNull(nameof(fieldInfo));
+
+            var declaringType = fieldInfo.DeclaringType;
+
+            var sourceParameter = Expression.Parameter(typeof(object), "source");
+            var valueParameter = Expression.Parameter(typeof(object), "value");
+
+            var sourceExpression = declaringType.IsValueType && !declaringType.IsNullableType()
+                ? Expression.Unbox(sourceParameter, declaringType)
+                : sourceParameter.CastOrConvertTo(declaringType);
+
+            var fieldExpression = Expression.Field(sourceExpression, fieldInfo);
+
+            var valueExpression = valueParameter.CastOrConvertTo(fieldExpression.Type);
+
+            var genericSetFieldMethodInfo = SetFieldMethodInfo.MakeGenericMethod(fieldExpression.Type);
+
+            var setFieldMethodCallExpression = Expression.Call(null, genericSetFieldMethodInfo, fieldExpression, valueExpression);
+
+            var setterFn = Expression
+                .Lambda<Action<object, object>>(setFieldMethodCallExpression, sourceParameter, valueParameter)
+                .Compile();
+
+            return setterFn;
+        }
+
+        public static Action<TType, object> CreateFieldSetter<TType>(FieldInfo fieldInfo)
+        {
+            _ = fieldInfo.WhenNotNull(nameof(fieldInfo));
+
+            var instance = Expression.Parameter(typeof(TType), "item");
+            var argument = Expression.Parameter(typeof(object), "arg");
+
+            var field = typeof(TType) != fieldInfo.DeclaringType
+                ? Expression.Field(Expression.TypeAs(instance, fieldInfo.DeclaringType), fieldInfo)
+                : Expression.Field(instance, fieldInfo);
+
+            var setterCall = Expression.Assign(
+                field,
+                Expression.Convert(argument, fieldInfo.FieldType));
+
+            return Expression
+                .Lambda<Action<TType, object>>(setterCall, instance, argument)
+                .Compile();
+        }
+
+        public static Action<TType, object> CreateFieldSetterByRef<TType>(FieldInfo fieldInfo)
+        {
+            _ = fieldInfo.WhenNotNull(nameof(fieldInfo));
+
+            var instance = Expression.Parameter(typeof(TType).MakeByRefType(), "item");
+            var argument = Expression.Parameter(typeof(object), "arg");
+
+            var field = typeof(TType) != fieldInfo.DeclaringType
+                ? Expression.Field(Expression.TypeAs(instance, fieldInfo.DeclaringType), fieldInfo)
+                : Expression.Field(instance, fieldInfo);
+
+            var setterCall = Expression.Assign(
+                field,
+                Expression.Convert(argument, fieldInfo.FieldType));
+
+            return Expression
+                .Lambda<Action<TType, object>>(setterCall, instance, argument)
+                .Compile();
+        }
+
+        internal static void SetField<TValue>(ref TValue field, TValue newValue) => field = newValue;
+    }
+}
---
+++ Tests/AllOverIt.Tests/Reflection/FieldHelperFixture.CreateFieldGetter.cs
@@ -0,0 +1,117 @@
+﻿using AllOverIt.Fixture;
+using AllOverIt.Fixture.Extensions;
+using AllOverIt.Reflection;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace AllOverIt.Tests.Reflection
+{
+    public partial class FieldHelperFixture : FixtureBase
+    {
+        private class DummyClass
+        {
+            public int Field1;
+            private int Field2;
+
+            public DummyClass()
+            {
+            }
+
+            public DummyClass(int field2)
+            {
+                Field2 = field2;
+            }
+        }
+
+        public class CreateFieldGetter_Object : FieldHelperFixture
+        {
+            [Fact]
+            public void Should_Throw_When_FieldInfo_Null()
+            {
+                Invoking(() =>
+                {
+                    _ = FieldHelper.CreateFieldGetter(null);
+                })
+                    .Should()
+                    .Throw<ArgumentNullException>()
+                    .WithNamedMessageWhenNull("fieldInfo");
+            }
+
+            [Fact]
+            public void Should_Create_Getter()
+            {
+                var expected = new DummyClass
+                {
+                    Field1 = Create<int>()
+                };
+
+                var fieldInfo = typeof(DummyClass).GetField(nameof(DummyClass.Field1));
+                var getter = FieldHelper.CreateFieldGetter(fieldInfo);
+
+                var actual = getter.Invoke(expected);
+
+                actual.Should().Be(expected.Field1);
+            }
+
+            [Fact]
+            public void Should_Create_Getter_For_Private_Field()
+            {
+                var field2 = Create<int>();
+                var model = new DummyClass(field2);
+
+                var fieldInfo = typeof(DummyClass).GetField("Field2");  // can't use nameof() since it is private
+                var getter = FieldHelper.CreateFieldGetter(fieldInfo);
+
+                var actual = getter.Invoke(model);
+
+                actual.Should().Be(field2);
+            }
+        }
+
+        public class CreateFieldGetter_Typed : FieldHelperFixture
+        {
+            [Fact]
+            public void Should_Throw_When_FieldInfo_Null()
+            {
+                Invoking(() =>
+                {
+                    _ = FieldHelper.CreateFieldGetter<DummyClass>(null);
+                })
+                    .Should()
+                    .Throw<ArgumentNullException>()
+                    .WithNamedMessageWhenNull("fieldInfo");
+            }
+
+            [Fact]
+            public void Should_Create_Getter()
+            {
+                var expected = new DummyClass
+                {
+                    Field1 = Create<int>()
+                };
+
+                var fieldInfo = typeof(DummyClass).GetField(nameof(DummyClass.Field1));
+                var getter = FieldHelper.CreateFieldGetter<DummyClass>(fieldInfo);
+
+                var actual = getter.Invoke(expected);
+
+                actual.Should().Be(expected.Field1);
+            }
+
+            [Fact]
+            public void Should_Create_Getter_For_Private_Field()
+            {
+                var field2 = Create<int>();
+                var model = new DummyClass(field2);
+
+                var fieldInfo = typeof(DummyClass).GetField("Field2");  // can't use nameof() since it is private
+                var getter = FieldHelper.CreateFieldGetter<DummyClass>(fieldInfo);
+
+                var actual = getter.Invoke(model);
+
+                actual.Should().Be(field2);
+            }
+        }
+    }
+}
