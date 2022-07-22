0586122cf381155f4056f6290d6b5f338c22fa80
Malcolm Smith <malcolm@mjfreelancing.com>
2022-07-22 17:30:21 +1000

    Added ExpressionUtils tests

--- Source/AllOverIt/Expressions/ExpressionUtils.cs
+++ Source/AllOverIt/Expressions/ExpressionUtils.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using AllOverIt.Assertion;
+using System;
 using System.Linq.Expressions;
 
 namespace AllOverIt.Expressions
 {
+    /// <summary>A utility class that provides general purpose expression helpers.</summary>
     public static class ExpressionUtils
     {
         private sealed class ParameterHolder
@@ -15,15 +17,27 @@ namespace AllOverIt.Expressions
             }
         }
 
-        // TODO: Tests
-
+        /// <summary>Wraps a value in a placeholder object and returns the value as a property of that object,
+        /// thus making it suitable for creating parameterized queryables.</summary>
+        /// <typeparam name="TType">The type of the value.</typeparam>
+        /// <param name="value">The value to be wrapped.</param>
+        /// <returns>The value, via a proxy property, as an <see cref="Expression"/>.</returns>
         public static Expression CreateParameterizedValue<TType>(TType value)
         {
             return CreateParameterizedValue(value, typeof(TType));
         }
 
-        public static Expression CreateParameterizedValue(object value, Type valueType)
+        /// <summary>Wraps a value in a placeholder object and returns the value as a property of that object,
+        /// thus making it suitable for creating parameterized queryables.</summary>
+        /// <param name="value">The value to be wrapped.</param>
+        /// <param name="valueType">The type of the value.</param>
+        /// <returns>The value, via a proxy property, as an <see cref="Expression"/>.</returns>
+        public static Expression CreateParameterizedValue(object value, Type valueType = default)
         {
+            valueType ??= value?.GetType();
+
+            Throw<ArgumentException>.WhenNull(valueType, "The value type must be provided when creating a parameterized value expression.");
+
             var parameterValue = new ParameterHolder(value);
             var constantParameter = Expression.Constant(parameterValue);
             var property = Expression.PropertyOrField(constantParameter, nameof(ParameterHolder.Value));
---
+++ Tests/AllOverIt.Tests/Expressions/ExpressionUtilsFixture.cs
@@ -0,0 +1,142 @@
+﻿using AllOverIt.Expressions;
+using AllOverIt.Extensions;
+using AllOverIt.Fixture;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace AllOverIt.Tests.Expressions
+{
+    public class ExpressionUtilsFixture : FixtureBase
+    {
+        public class CreateParameterizedValue_Typed : ExpressionUtilsFixture
+        {
+            [Fact]
+            public void Should_Return_Expression_For_Value()
+            {
+                var expected = Create<int>();
+
+                var actual = ExpressionUtils.CreateParameterizedValue<int>(expected);
+
+                actual.GetValue()
+                    .Should()
+                    .Be(expected);
+            }
+
+            [Fact]
+            public void Should_Return_Expression_For_Nullable_Value()
+            {
+                var actual = ExpressionUtils.CreateParameterizedValue<int?>(default);
+
+                actual.GetValue()
+                    .Should()
+                    .BeNull();
+            }
+
+            [Fact]
+            public void Should_Return_Expression_For_String()
+            {
+                var expected = Create<string>();
+
+                var actual = ExpressionUtils.CreateParameterizedValue<string>(expected);
+
+                actual.GetValue()
+                    .Should()
+                    .Be(expected);
+            }
+
+            [Fact]
+            public void Should_Return_Expression_For_Null_String()
+            {
+                var actual = ExpressionUtils.CreateParameterizedValue<string>(default);
+
+                actual.GetValue()
+                    .Should()
+                    .BeNull();
+            }
+        }
+
+        public class CreateParameterizedValue_Object : ExpressionUtilsFixture
+        {
+            [Fact]
+            public void Should_Throw_When_Type_Not_Provided()
+            {
+                Invoking(() =>
+                {
+                    _ = ExpressionUtils.CreateParameterizedValue((string) default, null);
+                })
+                    .Should()
+                    .Throw<ArgumentException>()
+                    .WithMessage("The value type must be provided when creating a parameterized value expression.");
+            }
+
+            [Fact]
+            public void Should_Determine_Value_Type()
+            {
+                var expected = Create<int>();
+
+                var actual = ExpressionUtils.CreateParameterizedValue(expected, null);
+
+                actual.GetValue()
+                    .Should()
+                    .Be(expected);
+            }
+
+            [Fact]
+            public void Should_Determine_String_Type()
+            {
+                object expected = Create<string>();
+
+                var actual = ExpressionUtils.CreateParameterizedValue(expected, null);
+
+                actual.GetValue()
+                    .Should()
+                    .Be(expected);
+            }
+
+            [Fact]
+            public void Should_Return_Expression_For_Value()
+            {
+                var expected = Create<int>();
+
+                var actual = ExpressionUtils.CreateParameterizedValue(expected, typeof(int));
+
+                actual.GetValue()
+                    .Should()
+                    .Be(expected);
+            }
+
+            [Fact]
+            public void Should_Return_Expression_For_Nullable_Value()
+            {
+                var actual = ExpressionUtils.CreateParameterizedValue(null, typeof(int?));
+
+                actual.GetValue()
+                    .Should()
+                    .BeNull();
+            }
+
+            [Fact]
+            public void Should_Return_Expression_For_String()
+            {
+                var expected = Create<string>();
+
+                var actual = ExpressionUtils.CreateParameterizedValue(expected, typeof(string));
+
+                actual.GetValue()
+                    .Should()
+                    .Be(expected);
+            }
+
+            [Fact]
+            public void Should_Return_Expression_For_Null_String()
+            {
+                var actual = ExpressionUtils.CreateParameterizedValue(null, typeof(string));
+
+                actual.GetValue()
+                    .Should()
+                    .BeNull();
+            }
+        }
+    }
+}
