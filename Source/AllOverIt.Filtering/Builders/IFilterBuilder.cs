b726f21e754fa95619b93af84913ffb5fe143607
Malcolm Smith <malcolm@mjfreelancing.com>
2022-07-23 21:58:06 +1000

    Added comments

--- Source/AllOverIt.Filtering/Builders/IFilterBuilder.cs
+++ Source/AllOverIt.Filtering/Builders/IFilterBuilder.cs
@@ -16,19 +16,47 @@ namespace AllOverIt.Filtering.Builders
        where TType : class
        where TFilter : class
     {
-        // Gets the current state of the filter builder as a specification.
-        ILinqSpecification<TType> AsSpecification { get; }
-
-        // Gets the current logical expression to cater for additional chaining.
+        /// <summary>
+        /// <para>
+        /// Gets the current logical expression to cater for additional of <see cref="Where(ILinqSpecification{TType})"/> or
+        /// <see cref="ILogicalFilterBuilder{TType, TFilter}.And(ILinqSpecification{TType})"/> or <see cref="ILogicalFilterBuilder{TType, TFilter}.Or(ILinqSpecification{TType})"/>
+        /// calls (or one of their overloads).
+        /// </para>
+        /// <para>
+        /// This property returns the current filter state. Subsequent calls to <see cref="ILogicalFilterBuilder{TType, TFilter}.And(ILinqSpecification{TType})"/> or
+        /// <see cref="ILogicalFilterBuilder{TType, TFilter}.Or(ILinqSpecification{TType})"/> (or one of their overloads) will apply the new expression as a binary operation
+        /// against <see cref="Current"/>.
+        /// </para>
+        /// </summary>
         ILogicalFilterBuilder<TType, TFilter> Current { get; }
 
+        /// <summary>Gets the state of the filter builder as an invokable specification.</summary>
+        ILinqSpecification<TType> AsSpecification();
+
+        /// <summary>Adds a filter operation to the filter builder, applying it is an equivalent specification. Multiple calls to this method
+        /// will result in the subsequent filter operations being applied as a binary AND operation.</summary>
+        /// <param name="propertyExpression">The expression specifying a string property on the <typeparamref name="TType"/> to apply the filter operation to.</param>
+        /// <param name="operation">An <see cref="IStringFilterOperation"/> filter operation to apply.</param>
+        /// <param name="options">An optional action to configure options that control how the specifications are built.</param>
+        /// <returns>A reference to the current filter builder so additional logical operations can be applied.</returns>
         ILogicalFilterBuilder<TType, TFilter> Where(Expression<Func<TType, string>> propertyExpression, Func<TFilter, IStringFilterOperation> operation,
             Action<OperationFilterOptions> options = default);
 
-        // Also handles IArrayFilterOperation
+        /// <summary>Adds a filter operation to the filter builder, applying it is an equivalent specification. Multiple calls to this method
+        /// will result in the subsequent filter operations being applied as a binary AND operation.</summary>
+        /// <typeparam name="TProperty">The property type for the specified property expression.</typeparam>
+        /// <param name="propertyExpression">The expression specifying a property on the <typeparamref name="TType"/> to apply the filter operation to.</param>
+        /// <param name="operation">An <see cref="IBasicFilterOperation"/> or <see cref="IArrayFilterOperation"/> filter operation to apply.</param>
+        /// <param name="options">An optional action to configure options that control how the specifications are built.</param>
+        /// <returns>A reference to the current filter builder so additional logical operations can be applied.</returns>
         ILogicalFilterBuilder<TType, TFilter> Where<TProperty>(Expression<Func<TType, TProperty>> propertyExpression, Func<TFilter, IBasicFilterOperation> operation,
             Action<OperationFilterOptions> options = default);
 
+        /// <summary>Adds a specification to the filter builder. Multiple calls to this method will result in the subsequent filter operations
+        /// being applied as a binary AND operation.</summary>
+        /// <param name="specification">The specification to apply to the filter builder. Multiple calls to this method will result in the
+        /// subsequent filter operations being applied as a binary AND operation.</param>
+        /// <returns>A reference to the current filter builder so additional logical operations can be applied.</returns>
         ILogicalFilterBuilder<TType, TFilter> Where(ILinqSpecification<TType> specification);
     }
 }
