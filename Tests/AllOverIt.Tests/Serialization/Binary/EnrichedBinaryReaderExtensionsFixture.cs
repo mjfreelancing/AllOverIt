149b5f28cdfe30a0c9b9c7d949ccf23f3c595cab
Malcolm Smith <malcolm@mjfreelancing.com>
2022-07-30 16:10:04 +1000

    Test updates

--- Tests/AllOverIt.Tests/Serialization/Binary/EnrichedBinaryReaderExtensionsFixture.cs
+++ Tests/AllOverIt.Tests/Serialization/Binary/EnrichedBinaryReaderExtensionsFixture.cs
@@ -1,4 +1,5 @@
-﻿using AllOverIt.Fixture;
+﻿using AllOverIt.Extensions;
+using AllOverIt.Fixture;
 using AllOverIt.Fixture.Extensions;
 using AllOverIt.Fixture.FakeItEasy;
 using AllOverIt.Serialization.Binary;
@@ -7,6 +8,8 @@ using AllOverIt.Serialization.Binary.Extensions;
 using FakeItEasy;
 using FluentAssertions;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace AllOverIt.Tests.Serialization.Binary
@@ -196,6 +199,38 @@ namespace AllOverIt.Tests.Serialization.Binary
                     .WithNamedMessageWhenNull("reader");
             }
 
+            [Fact]
+            public void Should_Throw_When_Unknown_Enum()
+            {
+                var enumTypeName = Create<string>();
+
+                _readerFake
+                    .CallsTo(fake => fake.ReadString())
+                    .Returns(enumTypeName);
+
+                Invoking(() =>
+                {
+                    _ = EnrichedBinaryReaderExtensions.ReadEnum<DummyEnum>(_readerFake.FakedObject);
+                })
+                   .Should()
+                   .Throw<BinaryReaderException>()
+                   .WithMessage($"Unknown enum type '{enumTypeName}'.");
+            }
+
+            [Fact]
+            public void Should_Read_Enum()
+            {
+                var value = Create<DummyEnum>();
+                var enumTypeName = typeof(DummyEnum).AssemblyQualifiedName;
+
+                _readerFake
+                    .CallsTo(fake => fake.ReadString())
+                    .ReturnsNextFromSequence(new[] { enumTypeName, $"{value}" });
+
+                var actual = EnrichedBinaryReaderExtensions.ReadEnum<DummyEnum>(_readerFake.FakedObject);
+
+                actual.Should().Be(value);
+            }
         }
 
         public class ReadObject_Typed : EnrichedBinaryReaderExtensionsFixture
@@ -212,6 +247,19 @@ namespace AllOverIt.Tests.Serialization.Binary
                     .WithNamedMessageWhenNull("reader");
             }
 
+            [Fact]
+            public void Should_Read_Object()
+            {
+                var value = Create<DummyType>();
+
+                _readerFake
+                    .CallsTo(fake => fake.ReadObject())
+                    .Returns(value);
+
+                var actual = EnrichedBinaryReaderExtensions.ReadObject<DummyType>(_readerFake.FakedObject);
+
+                actual.Should().Be(value);
+            }
         }
 
         public class ReadNullable : EnrichedBinaryReaderExtensionsFixture
@@ -228,6 +276,19 @@ namespace AllOverIt.Tests.Serialization.Binary
                     .WithNamedMessageWhenNull("reader");
             }
 
+            [Fact]
+            public void Should_Read_Nullablet()
+            {
+                int? value = Create<int>();
+
+                _readerFake
+                    .CallsTo(fake => fake.ReadObject())
+                    .Returns(value);
+
+                var actual = EnrichedBinaryReaderExtensions.ReadNullable<int>(_readerFake.FakedObject);
+
+                actual.Should().Be(value);
+            }
         }
 
         public class ReadEnumerable : EnrichedBinaryReaderExtensionsFixture
@@ -244,6 +305,25 @@ namespace AllOverIt.Tests.Serialization.Binary
                     .WithNamedMessageWhenNull("reader");
             }
 
+            [Fact]
+            public void Should_Read_Enumerable()
+            {
+                var values = new[] { 1, 0, 1, -1, 2 };
+
+                _readerFake
+                    .CallsTo(fake => fake.ReadInt32())
+                    .Returns(5);
+
+                _readerFake
+                    .CallsTo(fake => fake.ReadObject())
+                    .ReturnsNextFromSequence(1, 0, 1, -1, 2);
+
+                var actual = EnrichedBinaryReaderExtensions
+                    .ReadEnumerable(_readerFake.FakedObject)
+                    .Select(item => (int) item);
+
+                actual.SequenceEqual(values).Should().BeTrue();
+            }
         }
 
         public class ReadEnumumerable_Typed : EnrichedBinaryReaderExtensionsFixture
@@ -260,6 +340,20 @@ namespace AllOverIt.Tests.Serialization.Binary
                     .WithNamedMessageWhenNull("reader");
             }
 
+            [Fact]
+            public void Should_Read_Enumerable()
+            {
+                // must have List<object> to emulate what the write would have done
+                var values = new[] { 1, 0, 1, -1, 2 }.ToList();
+
+                _readerFake
+                    .CallsTo(fake => fake.ReadObject())
+                    .Returns(values.SelectAsList(item => (object)item));
+
+                var actual = EnrichedBinaryReaderExtensions.ReadEnumerable<int>(_readerFake.FakedObject);
+
+                actual.SequenceEqual(values).Should().BeTrue();
+            }
         }
 
         public class ReadDictionary : EnrichedBinaryReaderExtensionsFixture
@@ -276,6 +370,31 @@ namespace AllOverIt.Tests.Serialization.Binary
                     .WithNamedMessageWhenNull("reader");
             }
 
+            [Fact]
+            public void Should_Read_Dictionary()
+            {
+                var dictionary = new Dictionary<object, object>
+                {
+                    { Create<int>(), Create<string>() },
+                    { Create<double>(), Create<long>() },
+                    { Create<string>(), Create<double>() },
+                };
+
+                var keys = dictionary.Keys.ToList();
+                var values = dictionary.Values.ToList();
+
+                _readerFake
+                    .CallsTo(fake => fake.ReadInt32())
+                    .Returns(3);
+
+                _readerFake
+                    .CallsTo(fake => fake.ReadObject())
+                    .ReturnsNextFromSequence(keys[0], values[0], keys[1], values[1], keys[2], values[2]);
+
+                var actual = EnrichedBinaryReaderExtensions.ReadDictionary(_readerFake.FakedObject);
+
+                actual.Should().BeEquivalentTo(dictionary);
+            }
         }
 
         public class ReadDictionary_Typed : EnrichedBinaryReaderExtensionsFixture
@@ -292,6 +411,57 @@ namespace AllOverIt.Tests.Serialization.Binary
                     .WithNamedMessageWhenNull("reader");
             }
 
+            [Fact]
+            public void Should_Read_Dictionary_When_Object_Object()
+            {
+                var dictionary = new Dictionary<object, object>
+                {
+                    { Create<int>(), Create<string>() },
+                    { Create<double>(), Create<long>() },
+                    { Create<string>(), Create<double>() },
+                };
+
+                var keys = dictionary.Keys.ToList();
+                var values = dictionary.Values.ToList();
+
+                _readerFake
+                    .CallsTo(fake => fake.ReadInt32())
+                    .Returns(3);
+
+                _readerFake
+                    .CallsTo(fake => fake.ReadObject())
+                    .ReturnsNextFromSequence(keys[0], values[0], keys[1], values[1], keys[2], values[2]);
+
+                var actual = EnrichedBinaryReaderExtensions.ReadDictionary<object, object>(_readerFake.FakedObject);
+
+                actual.Should().BeEquivalentTo(dictionary);
+            }
+
+            [Fact]
+            public void Should_Read_Dictionary_When_Typed()
+            {
+                var dictionary = new Dictionary<int, string>
+                {
+                    { Create<int>(), Create<string>() },
+                    { Create<int>(), Create<string>() },
+                    { Create<int>(), Create<string>() },
+                };
+
+                var keys = dictionary.Keys.ToList();
+                var values = dictionary.Values.ToList();
+
+                _readerFake
+                    .CallsTo(fake => fake.ReadInt32())
+                    .Returns(3);
+
+                _readerFake
+                    .CallsTo(fake => fake.ReadObject())
+                    .ReturnsNextFromSequence(keys[0], values[0], keys[1], values[1], keys[2], values[2]);
+
+                var actual = EnrichedBinaryReaderExtensions.ReadDictionary<int, string>(_readerFake.FakedObject);
+
+                actual.Should().BeEquivalentTo(dictionary);
+            }
         }
     }
 }
