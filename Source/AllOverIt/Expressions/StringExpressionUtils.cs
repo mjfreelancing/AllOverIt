41fd9c00a95e36b4ac4ab8a3916eeba7dfe721d5
Malcolm Smith <malcolm@mjfreelancing.com>
2022-08-05 16:43:24 +1000

    Added CreateToLowerCallExpression() and CreateToUpperCallExpression()

--- Source/AllOverIt/Expressions/StringExpressionUtils.cs
+++ Source/AllOverIt/Expressions/StringExpressionUtils.cs
@@ -37,6 +37,10 @@ namespace AllOverIt.Expressions
         private static readonly MethodInfo EndsWithStringComparisonMethodInfo = CommonTypes.StringType.GetMethod(
             "EndsWith", new[] { CommonTypes.StringType, CommonTypes.StringComparisonType });
 
+        private static readonly MethodInfo ToLowerMethodInfo = CommonTypes.StringType.GetMethod("ToLower", Type.EmptyTypes);
+
+        private static readonly MethodInfo ToUpperMethodInfo = CommonTypes.StringType.GetMethod("ToUpper", Type.EmptyTypes);
+
         /// <summary>Creates a <see cref="MethodCallExpression"/> that will execute the static method <see cref="string.Compare(string?, string?)" /> or
         /// <see cref="string.Compare(string?, string?, StringComparison)"/> method based on the provided arguments.</summary>
         /// <param name="value1">The first string value.</param>
@@ -44,7 +48,7 @@ namespace AllOverIt.Expressions
         /// <param name="stringComparison">The <see cref="StringComparison"/> option, when provided.</param>
         /// <returns>A <see cref="MethodCallExpression"/> that will execute the static <see cref="string.Compare(string?, string?)" /> or
         /// <see cref="string.Compare(string?, string?, StringComparison)"/> method based on the provided arguments.</returns>
-        public static MethodCallExpression CreateCompareCallExpression(Expression value1, Expression value2, StringComparison? stringComparison)
+        public static MethodCallExpression CreateCompareCallExpression(Expression value1, Expression value2, StringComparison? stringComparison = default)
         {
             _ = value1.WhenNotNull(nameof(value1));
             _ = value2.WhenNotNull(nameof(value2));
@@ -61,7 +65,7 @@ namespace AllOverIt.Expressions
         /// <param name="stringComparison">The <see cref="StringComparison"/> option, when provided.</param>
         /// <returns>A <see cref="MethodCallExpression"/> that will execute the static <see cref="string.Contains(string)" /> or
         /// <see cref="string.Contains(string, StringComparison)"/> method based on the provided arguments.</returns>
-        public static MethodCallExpression CreateContainsCallExpression(Expression instance, Expression value, StringComparison? stringComparison)
+        public static MethodCallExpression CreateContainsCallExpression(Expression instance, Expression value, StringComparison? stringComparison = default)
         {
             _ = instance.WhenNotNull(nameof(instance));
             _ = value.WhenNotNull(nameof(value));
@@ -78,7 +82,7 @@ namespace AllOverIt.Expressions
         /// <param name="stringComparison">The <see cref="StringComparison"/> option, when provided.</param>
         /// <returns>A <see cref="MethodCallExpression"/> that will execute the static <see cref="string.StartsWith(string)" /> or
         /// <see cref="string.StartsWith(string, StringComparison)"/> method based on the provided arguments.</returns>
-        public static MethodCallExpression CreateStartsWithCallExpression(Expression instance, Expression value, StringComparison? stringComparison)
+        public static MethodCallExpression CreateStartsWithCallExpression(Expression instance, Expression value, StringComparison? stringComparison = default)
         {
             _ = instance.WhenNotNull(nameof(instance));
             _ = value.WhenNotNull(nameof(value));
@@ -95,7 +99,7 @@ namespace AllOverIt.Expressions
         /// <param name="stringComparison">The <see cref="StringComparison"/> option, when provided.</param>
         /// <returns>A <see cref="MethodCallExpression"/> that will execute the static <see cref="string.EndsWith(string)" /> or
         /// <see cref="string.EndsWith(string, StringComparison)"/> method based on the provided arguments.</returns>
-        public static MethodCallExpression CreateEndsWithCallExpression(Expression instance, Expression value, StringComparison? stringComparison)
+        public static MethodCallExpression CreateEndsWithCallExpression(Expression instance, Expression value, StringComparison? stringComparison = default)
         {
             _ = instance.WhenNotNull(nameof(instance));
             _ = value.WhenNotNull(nameof(value));
@@ -105,13 +109,35 @@ namespace AllOverIt.Expressions
                 : Expression.Call(instance, EndsWithMethodInfo, value);
         }
 
-        private static MethodCallExpression CreateStaticComparisonCallExpression(MethodInfo methodInfo, Expression instance, Expression value, StringComparison stringComparison)
+        /// <summary>Creates a <see cref="MethodCallExpression"/> that will execute the instance method <see cref="string.ToLower" />.</summary>
+        /// <param name="instance">The string instance to call the method on.</param>
+        /// <returns>A <see cref="MethodCallExpression"/> that will execute the instance method <see cref="string.ToLower" />.</returns>
+        public static MethodCallExpression CreateToLowerCallExpression(Expression instance)
+        {
+            _ = instance.WhenNotNull(nameof(instance));
+
+            return Expression.Call(instance, ToLowerMethodInfo);
+        }
+
+        /// <summary>Creates a <see cref="MethodCallExpression"/> that will execute the instance method <see cref="string.ToUpper" />.</summary>
+        /// <param name="instance">The string instance to call the method on.</param>
+        /// <returns>A <see cref="MethodCallExpression"/> that will execute the instance method <see cref="string.ToUpper" />.</returns>
+        public static MethodCallExpression CreateToUpperCallExpression(Expression instance)
+        {
+            _ = instance.WhenNotNull(nameof(instance));
+
+            return Expression.Call(instance, ToUpperMethodInfo);
+        }
+
+        private static MethodCallExpression CreateStaticComparisonCallExpression(MethodInfo methodInfo, Expression instance, Expression value,
+            StringComparison stringComparison)
         {
             var comparison = Expression.Constant(stringComparison);
             return Expression.Call(methodInfo, instance, value, comparison);
         }
 
-        private static MethodCallExpression CreateInstanceComparisonCallExpression(MethodInfo methodInfo, Expression instance, Expression value, StringComparison stringComparison)
+        private static MethodCallExpression CreateInstanceComparisonCallExpression(MethodInfo methodInfo, Expression instance, Expression value,
+            StringComparison stringComparison)
         {
             var comparison = Expression.Constant(stringComparison);
             return Expression.Call(instance, methodInfo, value, comparison);
