c1f9977fa4128afd0af110a9a99c9ea4ccb11325
Malcolm Smith <malcolm@mjfreelancing.com>
2022-08-08 09:16:54 +1000

    Correction to use static readonly

--- Source/AllOverIt/Expressions/Strings/StringComparisonMode.cs
+++ Source/AllOverIt/Expressions/Strings/StringComparisonMode.cs
@@ -7,31 +7,31 @@ namespace AllOverIt.Expressions.Strings
     public sealed class StringComparisonMode : EnrichedEnum<StringComparisonMode>
     {
         /// <summary>Apply no modification to string values.</summary>
-        public static StringComparisonMode None = new(0);
+        public static readonly StringComparisonMode None = new(0);
 
         /// <summary>Will lowercase input values (in advance) and lowercase each data row so string comparisons become case-insensitive.</summary>
-        public static StringComparisonMode ToLower = new(1);
+        public static readonly StringComparisonMode ToLower = new(1);
 
         /// <summary>Will uppercase input values (in advance) and uppercase each data row so string comparisons become case-insensitive.</summary>
-        public static StringComparisonMode ToUpper = new(2);
+        public static readonly StringComparisonMode ToUpper = new(2);
 
         /// <summary>Compare strings using culture-sensitive sort rules and the current culture.</summary>
-        public static StringComparisonMode CurrentCulture = new(3);
+        public static readonly StringComparisonMode CurrentCulture = new(3);
 
         /// <summary>Compare strings using culture-sensitive sort rules, the current culture, and ignoring the case of the strings being compared.</summary>
-        public static StringComparisonMode CurrentCultureIgnoreCase = new(4);
+        public static readonly StringComparisonMode CurrentCultureIgnoreCase = new(4);
 
         /// <summary>Compare strings using culture-sensitive sort rules and the invariant culture.</summary>
-        public static StringComparisonMode InvariantCulture = new(5);
+        public static readonly StringComparisonMode InvariantCulture = new(5);
 
         /// <summary>Compare strings using culture-sensitive sort rules, the invariant culture, and ignoring the case of the strings being compared.</summary>
-        public static StringComparisonMode InvariantCultureIgnoreCase = new(6);
+        public static readonly StringComparisonMode InvariantCultureIgnoreCase = new(6);
 
         /// <summary>Compare strings using ordinal (binary) sort rules.</summary>
-        public static StringComparisonMode Ordinal = new(7);
+        public static readonly StringComparisonMode Ordinal = new(7);
 
         /// <summary>Compare strings using ordinal (binary) sort rules and ignoring the case of the strings being compared.</summary>
-        public static StringComparisonMode OrdinalIgnoreCase = new(8);
+        public static readonly StringComparisonMode OrdinalIgnoreCase = new(8);
 
         public StringComparisonMode(int value, [CallerMemberName] string name = null)
             : base(value, name)
