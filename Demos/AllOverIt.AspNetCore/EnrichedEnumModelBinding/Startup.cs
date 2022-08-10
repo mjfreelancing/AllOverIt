44eda0dd81ccb7e17bac3706143f33d7b5d7be8f
Malcolm Smith <malcolm@mjfreelancing.com>
2022-08-10 13:43:54 +1000

    Cleanup

--- Demos/AllOverIt.AspNetCore/EnrichedEnumModelBinding/Startup.cs
+++ Demos/AllOverIt.AspNetCore/EnrichedEnumModelBinding/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace EnrichedEnumModelBinding
 {
