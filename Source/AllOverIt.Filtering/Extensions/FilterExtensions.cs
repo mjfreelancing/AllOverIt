33513a7eac69b45b306262b1dbe83ceaf460fdc7
Malcolm Smith <malcolm@mjfreelancing.com>
2022-08-26 16:01:45 +1000

    Start of filter extensions

---
+++ Source/AllOverIt.Filtering/Extensions/FilterExtensions.cs
@@ -0,0 +1,36 @@
+﻿using AllOverIt.Extensions;
+using AllOverIt.Filtering.Filters;
+using System.Linq;
+
+namespace AllOverIt.Filtering.Builders.Extensions
+{
+    /// <summary>Provides extension methods for various filter operations.</summary>
+    public static class FilterExtensions
+    {
+        /// <summary>Indicates if the filter has a non-null value with at least one element.</summary>
+        /// <typeparam name="TType">The filter element type.</typeparam>
+        /// <param name="filter">The filter instance.</param>
+        /// <returns>True if the filter has a non-null value with at least one element, otherwise false.</returns>
+        public static bool Any<TType>(this IArrayFilterOperation<TType> filter)
+        {
+            return filter.Value?.Any() ?? false;
+        }
+
+        /// <summary>Indicates if the filter has a non-null value that is not empty nor contains whitespace.</summary>
+        /// <param name="filter">The filter instance.</param>
+        /// <returns>True if the filter has a non-null value that is not empty nor contains whitespace, otherwise false.</returns>
+        public static bool Any(this IStringFilterOperation filter)
+        {
+            return filter.Value.IsNotNullOrEmpty();
+        }
+
+        /// <summary>Indicates if the filter has a non-null value.</summary>
+        /// <typeparam name="TType">The (nullable) filter type.</typeparam>
+        /// <param name="filter">The filter instance.</param>
+        /// <returns>True if the filter has a non-null value, otherwise false.</returns>
+        public static bool Any<TType>(this IBasicFilterOperation<TType?> filter) where TType : struct
+        {
+            return filter.Value.HasValue;
+        }
+    }
+}
---
+++ Tests/AllOverIt.Filtering.Tests/Extensions/FilterExtensionsFixture.cs
@@ -0,0 +1,35 @@
+﻿using AllOverIt.Filtering.Filters;
+using AllOverIt.Fixture;
+
+namespace AllOverIt.Filtering.Tests.Extensions
+{
+    public class FilterExtensionsFixture : FixtureBase
+    {
+        private class DummyClass
+        {
+            public int? Prop1 { get; }
+            public string Prop2 { get; }
+        }
+
+        private class DummyFilter
+        {
+            public class Prop1Filter
+            {
+                public EqualTo<int?> EqualTo { get; set; } = new();
+                public In<int?> In { get; set; } = new();
+            }
+
+            public class Prop2Filter
+            {
+                public EqualTo<string> EqualTo { get; set; } = new();
+                public In<string> In { get; set; } = new();
+            }
+
+            public Prop1Filter Prop1 { get; init; } = new();
+            public Prop2Filter Prop2 { get; init; } = new();
+        }
+
+
+
+    }
+}
