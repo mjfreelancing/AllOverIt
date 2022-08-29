e10457adc529ba65556c724d7dfc3151b6d20f08
Malcolm Smith <malcolm@mjfreelancing.com>
2022-08-29 19:19:54 +1000

    Added nullable types to AllOverIt.Reflection.CommonTypes

--- Source/AllOverIt/Reflection/CommonTypes.cs
+++ Source/AllOverIt/Reflection/CommonTypes.cs
@@ -17,41 +17,80 @@ namespace AllOverIt.Reflection
         /// <summary>Type declaration for a <c>typeof(bool)</c>.</summary>
         public static readonly Type BoolType = typeof(bool);
 
+        /// <summary>Type declaration for a <c>typeof(bool?)</c>.</summary>
+        public static readonly Type NullableBoolType = typeof(bool?);
+
         /// <summary>Type declaration for a <c>typeof(byte)</c>.</summary>
         public static readonly Type ByteType = typeof(byte);
+
+        /// <summary>Type declaration for a <c>typeof(byte?)</c>.</summary>
+        public static readonly Type NullableByteType = typeof(byte?);
 
         /// <summary>Type declaration for a <c>typeof(sbyte)</c>.</summary>
         public static readonly Type SByteType = typeof(sbyte);
 
+        /// <summary>Type declaration for a <c>typeof(sbyte?)</c>.</summary>
+        public static readonly Type NullableSByteType = typeof(sbyte?);
+
         /// <summary>Type declaration for a <c>typeof(ushort)</c>.</summary>
         public static readonly Type UShortType = typeof(ushort);
+
+        /// <summary>Type declaration for a <c>typeof(ushort?)</c>.</summary>
+        public static readonly Type NullableUShortType = typeof(ushort?);
 
         /// <summary>Type declaration for a <c>typeof(short)</c>.</summary>
         public static readonly Type ShortType = typeof(short);
 
+        /// <summary>Type declaration for a <c>typeof(short?)</c>.</summary>
+        public static readonly Type NullableShortType = typeof(short?);
+
         /// <summary>Type declaration for a <c>typeof(uint)</c>.</summary>
         public static readonly Type UIntType = typeof(uint);
+
+        /// <summary>Type declaration for a <c>typeof(uint?)</c>.</summary>
+        public static readonly Type NullableUIntType = typeof(uint?);
 
         /// <summary>Type declaration for a <c>typeof(int)</c>.</summary>
         public static readonly Type IntType = typeof(int);
 
+        /// <summary>Type declaration for a <c>typeof(int?)</c>.</summary>
+        public static readonly Type NullableIntType = typeof(int?);
+
         /// <summary>Type declaration for a <c>typeof(ulong)</c>.</summary>
         public static readonly Type ULongType = typeof(ulong);
+
+        /// <summary>Type declaration for a <c>typeof(ulong?)</c>.</summary>
+        public static readonly Type NullableULongType = typeof(ulong?);
 
         /// <summary>Type declaration for a <c>typeof(long)</c>.</summary>
         public static readonly Type LongType = typeof(long);
 
+        /// <summary>Type declaration for a <c>typeof(long?)</c>.</summary>
+        public static readonly Type NullableLongType = typeof(long?);
+
         /// <summary>Type declaration for a <c>typeof(float)</c>.</summary>
         public static readonly Type FloatType = typeof(float);
+
+        /// <summary>Type declaration for a <c>typeof(float?)</c>.</summary>
+        public static readonly Type NullableFloatType = typeof(float?);
 
         /// <summary>Type declaration for a <c>typeof(double)</c>.</summary>
         public static readonly Type DoubleType = typeof(double);
 
+        /// <summary>Type declaration for a <c>typeof(double?)</c>.</summary>
+        public static readonly Type NullableDoubleType = typeof(double?);
+
         /// <summary>Type declaration for a <c>typeof(decimal)</c>.</summary>
         public static readonly Type DecimalType = typeof(decimal);
 
+        /// <summary>Type declaration for a <c>typeof(decimal?)</c>.</summary>
+        public static readonly Type NullableDecimalType = typeof(decimal?);
+
         /// <summary>Type declaration for a <c>typeof(char)</c>.</summary>
         public static readonly Type CharType = typeof(char);
+
+        /// <summary>Type declaration for a <c>typeof(char?)</c>.</summary>
+        public static readonly Type NullableCharType = typeof(char?);
 
         /// <summary>Type declaration for a <c>typeof(Enum)</c>.</summary>
         public static readonly Type EnumType = typeof(Enum);
@@ -59,11 +98,20 @@ namespace AllOverIt.Reflection
         /// <summary>Type declaration for a <c>typeof(Guid)</c>.</summary>
         public static readonly Type GuidType = typeof(Guid);
 
+        /// <summary>Type declaration for a <c>typeof(Guid?)</c>.</summary>
+        public static readonly Type NullableGuidType = typeof(Guid?);
+
         /// <summary>Type declaration for a <c>typeof(DateTime)</c>.</summary>
         public static readonly Type DateTimeType = typeof(DateTime);
 
+        /// <summary>Type declaration for a <c>typeof(DateTime)</c>.</summary>
+        public static readonly Type NullableDateTimeType = typeof(DateTime?);
+
         /// <summary>Type declaration for a <c>typeof(TimeSpan)</c>.</summary>
         public static readonly Type TimeSpanType = typeof(TimeSpan);
+
+        /// <summary>Type declaration for a <c>typeof(TimeSpan)</c>.</summary>
+        public static readonly Type NullableTimeSpanType = typeof(TimeSpan?);
 
         /// <summary>Type declaration for a <c>typeof(Nullable&lt;&gt;)</c>.</summary>
         public static readonly Type NullableGenericType = typeof(Nullable<>);
