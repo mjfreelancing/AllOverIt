27fcd8f4857e208699bd65a7464a785e91497089
Malcolm Smith <malcolm@mjfreelancing.com>
2022-09-09 13:43:44 +1000

    Comment fix

--- Source/AllOverIt/Mapping/ObjectMapperOptions.cs
+++ Source/AllOverIt/Mapping/ObjectMapperOptions.cs
@@ -6,8 +6,8 @@
         /// <summary>
         /// <para>The default mapping behaviour of collections when the source is null is to create an empty array, list, or
         /// dictionary. This option changes that behaviour so a null source value is mapped as a null target value.</para>
-        /// <para>If the target collection cannot be assigned an array, list, or dictionary then the <see cref="WithConversion(string, Func{IObjectMapper, object, object})"/>
-        /// method should be used.</para>
+        /// <para>If the target collection cannot be assigned an array, list, or dictionary then the mapper should be configured
+        /// to construct or convert the source value to the required type.</para>
         /// </summary>
         public bool AllowNullCollections { get; set; }
     }
