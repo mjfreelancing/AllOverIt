35a9d715fdae4c895d13f0c674d9418cdb3f13b7
Malcolm Smith <malcolm@mjfreelancing.com>
2023-08-30 20:06:33 +1000

    More tests

---
+++ Tests/AllOverIt.Cryptography.Tests/Hybrid/RsaSigningConfigurationFixture.cs
@@ -0,0 +1,39 @@
+﻿using AllOverIt.Cryptography.Hybrid;
+using AllOverIt.Cryptography.RSA;
+using AllOverIt.Fixture;
+using FluentAssertions;
+using System.Security.Cryptography;
+using Xunit;
+
+namespace AllOverIt.Cryptography.Tests.Hybrid
+{
+    public class RsaSigningConfigurationFixture : FixtureBase
+    {
+        public class Constructor : RsaSigningConfigurationFixture
+        {
+            [Fact]
+            public void Should_Have_Defaults()
+            {
+                var actual = new RsaSigningConfiguration();
+
+                actual.Keys.Should().NotBeNull();
+                actual.HashAlgorithmName.Should().Be(HashAlgorithmName.SHA256);
+                actual.Padding.Should().Be(RSASignaturePadding.Pkcs1);
+            }
+        }
+
+        public class Constructor_Keys : RsaSigningConfigurationFixture
+        {
+            [Fact]
+            public void Should_Have_Defaults_With_Keys()
+            {
+                var keys = RsaKeyPair.Create();
+                var actual = new RsaSigningConfiguration(keys);
+
+                actual.Keys.Should().BeSameAs(keys);
+                actual.HashAlgorithmName.Should().Be(HashAlgorithmName.SHA256);
+                actual.Padding.Should().Be(RSASignaturePadding.Pkcs1);
+            }
+        }
+    }
+}
