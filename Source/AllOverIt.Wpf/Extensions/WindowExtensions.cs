b62f9813d29c4e546239624eb577438fa88163ab
Malcolm Smith <malcolm@mjfreelancing.com>
2023-09-07 23:19:59 +1000

    Cleanup

--- Source/AllOverIt.Wpf/Extensions/WindowExtensions.cs
+++ Source/AllOverIt.Wpf/Extensions/WindowExtensions.cs
@@ -6,6 +6,9 @@ namespace AllOverIt.Wpf.Extensions
     /// <summary>Provides several useful extension methods for use with <see cref="Window"/>.</summary>
     public static class WindowExtensions
     {
+        /// <summary>Gets a <see cref="WindowWrapper"/> for a specified <see cref="Window"/>.</summary>
+        /// <param name="window">The window to be wrapped.</param>
+        /// <returns>A <see cref="WindowWrapper"/> for a specified <see cref="Window"/>.</returns>
         public static WindowWrapper WrapWindow(this Window window)
         {
             return new WindowWrapper(window);
