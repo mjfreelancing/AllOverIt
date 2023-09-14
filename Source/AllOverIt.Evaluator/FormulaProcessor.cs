f531f7cb7948af6e3428cda79b0fd06b5058fc35
Malcolm Smith <malcolm@mjfreelancing.com>
2023-09-15 08:17:20 +1000

    cleanup

--- Source/AllOverIt.Evaluator/FormulaProcessor.cs
+++ Source/AllOverIt.Evaluator/FormulaProcessor.cs
@@ -6,8 +6,6 @@ using AllOverIt.Evaluator.Variables;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
