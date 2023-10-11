beb00fb0897bafbc90cddef94907e3db475d0fd3
Malcolm Smith <malcolm@mjfreelancing.com>
2023-10-11 17:03:05 +1100

    Update filtering demo with a value type

---
+++ Demos/AllOverIt.Filtering/FilteringDemo/DateTimeValue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace FilteringDemo
+{
+    public sealed class DateTimeValue
+    {
+        public DateTime Value { get; set; }
+
+        public static implicit operator DateTime?(DateTimeValue dateTimeValue) => dateTimeValue?.Value;
+        public static explicit operator DateTimeValue(DateTime value) => new() { Value = value };
+
+        public static bool operator >=(DateTimeValue left, DateTimeValue right)
+        {
+            if (left == null || right == null)
+            {
+                return false;
+            }
+
+            return left.Value >= right.Value;
+        }
+
+        public static bool operator <=(DateTimeValue left, DateTimeValue right)
+        {
+            if (left == null || right == null)
+            {
+                return false;
+            }
+
+            return left.Value <= right.Value;
+        }
+
+        public static bool operator >(DateTimeValue left, DateTimeValue right)
+        {
+            if (left == null || right == null)
+            {
+                return false;
+            }
+
+            return left.Value > right.Value;
+        }
+
+        public static bool operator <(DateTimeValue left, DateTimeValue right)
+        {
+            if (left == null || right == null)
+            {
+                return false;
+            }
+
+            return left.Value < right.Value;
+        }
+    }
+}
--- Demos/AllOverIt.Filtering/FilteringDemo/Product.cs
+++ Demos/AllOverIt.Filtering/FilteringDemo/Product.cs
@@ -1,13 +1,12 @@
-﻿using System;
-
-namespace FilteringDemo
+﻿namespace FilteringDemo
 {
+
     internal sealed class Product
     {
         public bool Active { get; set; }
         public string Category { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
-        public DateTime LastUpdated { get; set; }
+        public DateTimeValue LastUpdated { get; set; }
     }
 }
