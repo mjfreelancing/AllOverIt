3ebd5ec805070a92c7222bfe7bb2990891608fbf
Malcolm Smith <malcolm@mjfreelancing.com>
2023-12-09 01:19:15 +1100

    Added initial circular buffer

---
+++ Source/AllOverIt/Collections/CircularBuffer.cs
@@ -0,0 +1,244 @@
+﻿using AllOverIt.Assertion;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AllOverIt.Collections
+{
+    /// <summary>Implements a circular buffer of a fixed size, providing O(1) performance for access,
+    /// insert, pop, and push operations.</summary>
+    public class CircularBuffer<TType> : IEnumerable<TType>
+    {
+        private readonly TType[] _buffer;
+
+        private int _start;
+        private int _end;
+        private int _size;
+
+        public int Size => _size;
+
+        public int Capacity => _buffer.Length;
+
+        public bool IsFull => Size == Capacity;
+
+        public bool IsEmpty => Size == 0;
+
+        public CircularBuffer(int capacity)
+            : this(capacity, [])
+        {
+        }
+
+        public CircularBuffer(int capacity, TType[] items)
+        {
+            _ = items.WhenNotNull(nameof(items));
+
+            Throw<ArgumentException>.When(capacity < 1, "The circular buffer requires a capacity of at least 1.");
+            Throw<ArgumentException>.When(items.Length > capacity, "The item count exceeds the circular buffer capacity.");
+
+            _buffer = new TType[capacity];
+
+            Array.Copy(items, _buffer, items.Length);
+
+            _size = items.Length;
+            _start = 0;
+            _end = _size == capacity ? 0 : _size;
+        }
+
+        public TType Front()
+        {
+            Throw<InvalidOperationException>.When(IsEmpty, "The buffer is empty.");
+
+            return _buffer[_start];
+        }
+
+        public TType Back()
+        {
+            Throw<InvalidOperationException>.When(IsEmpty, "The buffer is empty.");
+
+            return _buffer[(_end != 0 ? _end : Capacity) - 1];
+        }
+
+        public TType this[int index]
+        {
+            get
+            {
+                var actualIndex = InternalIndex(index);
+
+                return _buffer[actualIndex];
+            }
+            set
+            {
+                var actualIndex = InternalIndex(index);
+
+                _buffer[actualIndex] = value;
+            }
+        }
+
+        public void PushFront(TType item)
+        {
+            if (IsFull)
+            {
+                DecrementWithWrap(ref _start);
+                _end = _start;
+                _buffer[_start] = item;
+            }
+            else
+            {
+                DecrementWithWrap(ref _start);
+                _buffer[_start] = item;
+                ++_size;
+            }
+        }
+
+        public void PushBack(TType item)
+        {
+            if (IsFull)
+            {
+                _buffer[_end] = item;
+                IncrementWithWrap(ref _end);
+                _start = _end;
+            }
+            else
+            {
+                _buffer[_end] = item;
+                IncrementWithWrap(ref _end);
+                ++_size;
+            }
+        }
+
+        public void PopFront()
+        {
+            Throw<InvalidOperationException>.When(IsEmpty, "The buffer is empty");
+
+            _buffer[_start] = default;
+            IncrementWithWrap(ref _start);
+            --_size;
+        }
+
+        public void PopBack()
+        {
+            Throw<InvalidOperationException>.When(IsEmpty, "The buffer is empty");
+
+            DecrementWithWrap(ref _end);
+            _buffer[_end] = default;
+            --_size;
+        }
+
+        public void Clear()
+        {
+            _start = 0;
+            _end = 0;
+            _size = 0;
+
+            Array.Clear(_buffer, 0, _buffer.Length);
+        }
+
+        public TType[] ToArray()
+        {
+            var array = new TType[Size];
+            var offset = 0;
+            var segments = GetArraySegments();
+
+            foreach (var segment in segments)
+            {
+                Array.Copy(segment.Array, segment.Offset, array, offset, segment.Count);
+                offset += segment.Count;
+            }
+
+            return array;
+        }
+
+        public IEnumerator<TType> GetEnumerator()
+        {
+            var segments = GetArraySegments();
+
+            foreach (var segment in segments)
+            {
+                foreach (var element in segment)
+                {
+                    yield return element;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private void IncrementWithWrap(ref int index)
+        {
+            if (++index == Capacity)
+            {
+                index = 0;
+            }
+        }
+
+        private void DecrementWithWrap(ref int index)
+        {
+            if (index == 0)
+            {
+                index = Capacity;
+            }
+
+            index--;
+        }
+
+        private int InternalIndex(int index)
+        {
+            Throw<IndexOutOfRangeException>.When(index >= _size, "The provided index exceeds the current buffer size.");
+
+            var offset = index < (Capacity - _start)
+                ? index
+                : index - Capacity;
+
+            return _start + offset;
+        }
+
+
+
+
+        /// <summary>
+        /// Get the contents of the buffer as 2 ArraySegments.
+        /// Respects the logical contents of the buffer, where
+        /// each segment and items in each segment are ordered
+        /// according to insertion.
+        ///
+        /// Fast: does not copy the array elements.
+        /// Useful for methods like <c>Send(IList&lt;ArraySegment&lt;Byte&gt;&gt;)</c>.
+        /// 
+        /// <remarks>Segments may be empty.</remarks>
+        /// </summary>
+        /// <returns>An IList with 2 segments corresponding to the buffer content.</returns>
+        private IEnumerable<ArraySegment<TType>> GetArraySegments()
+        {
+            // The array is composed by at most two non-contiguous segments, 
+
+            if (IsEmpty)
+            {
+                yield return new ArraySegment<TType>([]);
+            }
+            else
+            {
+                yield return FirstSegment();
+                yield return SecondSegment();
+            }
+        }
+
+        private ArraySegment<TType> FirstSegment()
+        {
+            var count = _start < _end
+                ? _end - _start
+                : _buffer.Length - _start;
+
+            return new ArraySegment<TType>(_buffer, _start, count);
+        }
+
+        private ArraySegment<TType> SecondSegment()
+        {
+            return _start < _end
+                ? new ArraySegment<TType>([])
+                : new ArraySegment<TType>(_buffer, 0, _end);
+        }
+    }
+}
---
+++ Tests/AllOverIt.Tests/Collections/CircularBufferFixture.cs
@@ -0,0 +1,319 @@
+﻿using AllOverIt.Collections;
+using AllOverIt.Fixture;
+using AllOverIt.Fixture.Extensions;
+using FluentAssertions;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace AllOverIt.Tests.Collections
+{
+    public class CircularBufferFixture : FixtureBase
+    {
+        public class Constructor_Capacity : CircularBufferFixture
+        {
+            [Fact]
+            public void Should_Throw_When_Invalid_Capacity()
+            {
+                var capacity = GetWithinRange(-1, 0);
+
+                Invoking(() =>
+                {
+                    _ = new CircularBuffer<int>(capacity);
+                })
+                .Should()
+                .Throw<ArgumentException>()
+                .WithMessage("The circular buffer requires a capacity of at least 1.");
+            }
+
+            [Fact]
+            public void Should_Initialize()
+            {
+                var capacity = GetWithinRange(1, 10);
+
+                var buffer = new CircularBuffer<int>(capacity);
+
+                buffer.Size.Should().Be(0);
+                buffer.Capacity.Should().Be(capacity);
+                buffer.IsEmpty.Should().BeTrue();
+                buffer.IsFull.Should().BeFalse();
+            }
+        }
+
+        public class Constructor_Capacity_Items : CircularBufferFixture
+        {
+            [Fact]
+            public void Should_Throw_When_Invalid_Capacity()
+            {
+                var capacity = GetWithinRange(-1, 0);
+
+                Invoking(() =>
+                {
+                    _ = new CircularBuffer<int>(capacity, CreateMany<int>().ToArray());
+                })
+                .Should()
+                .Throw<ArgumentException>()
+                .WithMessage("The circular buffer requires a capacity of at least 1.");
+            }
+
+            [Fact]
+            public void Should_Throw_When_Items_Null()
+            {
+                Invoking(() =>
+                {
+                    _ = new CircularBuffer<int>(Create<int>(), null);
+                })
+                .Should()
+                .Throw<ArgumentNullException>()
+                .WithNamedMessageWhenNull("items");
+            }
+
+            [Fact]
+            public void Should_Throw_When_Items_Too_Long()
+            {
+                Invoking(() =>
+                {
+                    var capacity = Create<int>();
+
+                    _ = new CircularBuffer<int>(capacity, CreateMany<int>(capacity + 1).ToArray());
+                })
+                .Should()
+                .Throw<ArgumentException>()
+                .WithMessage("The item count exceeds the circular buffer capacity.");
+            }
+
+            [Fact]
+            public void Should_Initialize_Default()
+            {
+                var capacity = GetWithinRange(1, 10);
+
+                var buffer = new CircularBuffer<int>(capacity);
+
+                buffer.Size.Should().Be(0);
+                buffer.Capacity.Should().Be(capacity);
+                buffer.IsEmpty.Should().BeTrue();
+                buffer.IsFull.Should().BeFalse();
+            }
+
+            [Fact]
+            public void Should_Initialize_Some_Items()
+            {
+                var capacity = GetWithinRange(5, 10);
+                var size = capacity - 3;
+                var items = CreateMany<int>(size).ToArray();
+
+                var buffer = new CircularBuffer<int>(capacity, items);
+
+                buffer.Size.Should().Be(size);
+                buffer.Capacity.Should().Be(capacity);
+                buffer.IsEmpty.Should().BeFalse();
+                buffer.IsFull.Should().BeFalse();
+            }
+
+            [Fact]
+            public void Should_Initialize_Full_Items()
+            {
+                var capacity = GetWithinRange(5, 10);
+                var items = CreateMany<int>(capacity).ToArray();
+
+                var buffer = new CircularBuffer<int>(capacity, items);
+
+                buffer.Size.Should().Be(capacity);
+                buffer.Capacity.Should().Be(capacity);
+                buffer.IsEmpty.Should().BeFalse();
+                buffer.IsFull.Should().BeTrue();
+            }
+        }
+
+        public class Front : CircularBufferFixture
+        {
+            [Fact]
+            public void Should_Throw_When_Empty()
+            {
+                Invoking(() =>
+                {
+                    var buffer = new CircularBuffer<int>(Create<int>());
+                    buffer.Front();
+                })
+                .Should()
+                .Throw<InvalidOperationException>()
+                .WithMessage("The buffer is empty.");
+            }
+
+            [Fact]
+            public void Should_Get_Front_After_Push_Front()
+            {
+                var buffer = new CircularBuffer<int>(3);
+
+                for (var i = 0; i < 5; i++)
+                {
+                    var expected = Create<int>();
+
+                    buffer.PushFront(expected);
+
+                    var actual = buffer.Front();
+
+                    actual.Should().Be(expected);
+                }
+            }
+
+            [Fact]
+            public void Should_Get_Front_After_Push_Back()
+            {
+                var expected = CreateMany<int>(3);
+                var buffer = new CircularBuffer<int>(3);
+
+                for (var i = 0; i < 3; i++)
+                {
+                    buffer.PushBack(expected[i]);
+
+                    var actual = buffer.Front();
+
+                    actual.Should().Be(expected[0]);
+                }
+
+                buffer.PushBack(Create<int>());
+                buffer.Front().Should().Be(expected[1]);
+
+                buffer.PushBack(Create<int>());
+                buffer.Front().Should().Be(expected[2]);
+            }
+        }
+
+        public class Back : CircularBufferFixture
+        {
+            [Fact]
+            public void Should_Throw_When_Empty()
+            {
+                Invoking(() =>
+                {
+                    var buffer = new CircularBuffer<int>(Create<int>());
+                    buffer.Back();
+                })
+                .Should()
+                .Throw<InvalidOperationException>()
+                .WithMessage("The buffer is empty.");
+            }
+
+            [Fact]
+            public void Should_Get_Back_After_Push_Front()
+            {
+                var expected = CreateMany<int>(3);
+                var buffer = new CircularBuffer<int>(3);
+
+                for (var i = 0; i < 3; i++)
+                {
+                    buffer.PushFront(expected[i]);
+
+                    var actual = buffer.Back();
+
+                    actual.Should().Be(expected[0]);
+                }
+
+                buffer.PushFront(Create<int>());
+                buffer.Back().Should().Be(expected[1]);
+
+                buffer.PushFront(Create<int>());
+                buffer.Back().Should().Be(expected[2]);
+            }
+
+            [Fact]
+            public void Should_Get_Back_After_Push_Back()
+            {
+                var buffer = new CircularBuffer<int>(3);
+
+                for (var i = 0; i < 5; i++)
+                {
+                    var expected = Create<int>();
+
+                    buffer.PushBack(expected);
+
+                    var actual = buffer.Back();
+
+                    actual.Should().Be(expected);
+                }
+            }
+        }
+
+        public class Items : CircularBufferFixture
+        {
+            [Fact]
+            public void Should_Throw_When_Empty()
+            {
+                Invoking(() =>
+                {
+                    var buffer = new CircularBuffer<int>(Create<int>());
+
+                    _ = buffer[1];
+                })
+                .Should()
+                .Throw<IndexOutOfRangeException>()
+                .WithMessage("The provided index exceeds the current buffer size.");
+            }
+
+            [Fact]
+            public void Should_Get_At_Index()
+            {
+                var expected = CreateMany<int>(3).ToArray();
+                var buffer = new CircularBuffer<int>(4, expected);
+
+                for (var i = 0; i < 3; i++)
+                {
+                    var actual = buffer[i];
+
+                    actual.Should().Be(expected[i]);
+                }
+            }
+
+            [Fact]
+            public void Should_Throw_When_Index_Out_Of_Range()
+            {
+                Invoking(() =>
+                {
+                    var expected = CreateMany<int>(3).ToArray();
+                    var buffer = new CircularBuffer<int>(4, expected);
+
+                    _ = buffer[3];
+                })
+                .Should()
+                .Throw<IndexOutOfRangeException>()
+                .WithMessage("The provided index exceeds the current buffer size.");
+            }
+        }
+
+        public class PushFront : CircularBufferFixture
+        {
+
+        }
+
+        public class PushBack : CircularBufferFixture
+        {
+
+        }
+
+        public class PopFront : CircularBufferFixture
+        {
+
+        }
+
+        public class PopBack : CircularBufferFixture
+        {
+
+        }
+
+        public class Clear : CircularBufferFixture
+        {
+
+        }
+
+        public class ToArray : CircularBufferFixture
+        {
+
+        }
+
+        public class GetEnumerator : CircularBufferFixture
+        {
+
+        }
+    }
+}
