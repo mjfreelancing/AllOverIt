2f6b49d7cffd444d796aff95864e5d19384304b9
Malcolm Smith <malcolm@mjfreelancing.com>
2023-12-09 22:53:28 +1100

    Cleanup

--- Demos/AllOverIt/Aspects/InterceptorDemo/Interceptors/GetSecretHandler.cs
+++ Demos/AllOverIt/Aspects/InterceptorDemo/Interceptors/GetSecretHandler.cs
@@ -5,16 +5,6 @@ using System.Reflection;
 
 namespace InterceptorDemo.Interceptors
 {
-    internal sealed class GetSecretIdHandler : InterceptorHandlerBase<int>
-    {
-        public override MethodInfo[] TargetMethods { get; } = [typeof(ISecretService).GetMethod(nameof(ISecretService.GetSecretId))];
-
-        protected override InterceptorState BeforeInvoke(MethodInfo targetMethod, ref object[] args, ref int result)
-        {
-            return base.BeforeInvoke(targetMethod, ref args, ref result);
-        }
-    }
-
     internal sealed class GetSecretHandler : InterceptorHandlerBase<string>
     {
         private readonly long _minimimReportableMilliseconds;
---
+++ Demos/AllOverIt/Aspects/InterceptorDemo/Interceptors/GetSecretIdHandler.cs
@@ -0,0 +1,15 @@
+﻿using AllOverIt.Aspects;
+using System.Reflection;
+
+namespace InterceptorDemo.Interceptors
+{
+    internal sealed class GetSecretIdHandler : InterceptorHandlerBase<int>
+    {
+        public override MethodInfo[] TargetMethods { get; } = [typeof(ISecretService).GetMethod(nameof(ISecretService.GetSecretId))];
+
+        protected override InterceptorState BeforeInvoke(MethodInfo targetMethod, ref object[] args, ref int result)
+        {
+            return base.BeforeInvoke(targetMethod, ref args, ref result);
+        }
+    }
+}
