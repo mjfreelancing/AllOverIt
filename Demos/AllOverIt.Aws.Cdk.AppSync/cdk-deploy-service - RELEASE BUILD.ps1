57bb7f460b68e5cd7b759f68cff13cd661f0bbc6
Malcolm Smith <malcolm@mjfreelancing.com>
2024-02-29 23:35:01 +1100

    Cleanup

--- Demos/AllOverIt.Aws.Cdk.AppSync/cdk-deploy-service - RELEASE BUILD.ps1
+++ Demos/AllOverIt.Aws.Cdk.AppSync/cdk-deploy-service - RELEASE BUILD.ps1
@@ -5,5 +5,5 @@ Clear-History
 cls
 $env:GetAllContinents = 'https://www.google.com'
 cdk bootstrap "aws://550269505143/ap-southeast-2" 
-cdk deploy --app "dotnet exec ./GraphqlSchema/bin/Release/net6.0/GraphqlSchema.dll" --require-approval never --verbose --all
+cdk deploy --app "dotnet exec ./GraphqlSchema/bin/Release/net8.0/GraphqlSchema.dll" --require-approval never --verbose --all
 pause
