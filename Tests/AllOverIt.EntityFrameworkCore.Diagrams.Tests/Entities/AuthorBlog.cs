1b1165c0830d81ce7b0b6d44dcbb84236e19267a
Malcolm Smith <malcolm@mjfreelancing.com>
2024-03-05 22:46:33 +1100

    Started adding entity framework diagram tests

---
+++ Tests/AllOverIt.EntityFrameworkCore.Diagrams.Tests/Entities/AuthorBlog.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AllOverIt.EntityFrameworkCore.Diagrams.Tests.Entities
+{
+    [Table(nameof(AuthorBlog))]       // Enforce the name rather than take on the DbSet<> property name
+    public class AuthorBlog
+    {
+        public int Id { get; set; }
+
+        // Deliberately using a name that is different from the type, and before 'Author' to test preserving column order
+        [Required]
+        public Blog Blogger { get; set; }
+
+        [Required]
+        public Author Author { get; set; }
+    }
+}
---
+++ Tests/AllOverIt.EntityFrameworkCore.Diagrams.Tests/Entities/Comment.cs
@@ -0,0 +1,22 @@
+﻿using AllOverIt.EntityFrameworkCore.Diagrams.Tests.Attributes;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AllOverIt.EntityFrameworkCore.Diagrams.Tests.Entities
+{
+    [Table(nameof(Comment))]       // Enforce the name rather than take on the DbSet<> property name
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [LongString]
+        public string Content { get; set; }
+
+        [Required]
+        public Post Post { get; set; }
+
+        [Required]
+        public Author Author { get; set; }
+    }
+}
