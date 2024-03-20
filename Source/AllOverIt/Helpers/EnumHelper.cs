90ccef7f124ce797ae996e5b1abc26c28e54d353
Malcolm Smith <malcolm@mjfreelancing.com>
2024-03-20 19:21:27 +1100

    Added new methods to extract an array of enum values

--- Source/AllOverIt/Helpers/EnumHelper.cs
+++ Source/AllOverIt/Helpers/EnumHelper.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+
+#if NET7_0_OR_GREATER
 using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.InteropServices;
+#endif
 
 namespace AllOverIt.Helpers
 {
@@ -9,10 +14,76 @@ namespace AllOverIt.Helpers
         /// <summary>Returns all possible enum values.</summary>
         /// <typeparam name="TType">The Enum type.</typeparam>
         /// <returns>All possible enum values.</returns>
-        public static IReadOnlyCollection<TType> GetEnumValues<TType>()
-          where TType : struct, Enum
+        public static TEnumType[] GetEnumValues<TEnumType>()
+          where TEnumType : Enum
         {
-            return (TType[])Enum.GetValues(typeof(TType));
+            return (TEnumType[]) Enum.GetValues(typeof(TEnumType));
         }
+
+#if NET7_0_OR_GREATER
+        /// <summary>Given a bit mask as a numeric value, get an array of corresponding enum values for a given
+        /// enumeration type. It is assumed each enum value will be a multiple of two.</summary>
+        /// <typeparam name="TEnumType">The enumeration type.</typeparam>
+        /// <typeparam name="TMaskType">The numeric type containing the bit mask.</typeparam>
+        /// <param name="mask">The value of the bit mask.</param>
+        /// <returns>An array of enum values for a given bit mask value.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">If the numeric value contains a set bit that does not correspond to an enum value.</exception>
+        public static TEnumType[] GetValuesFromBitMask<TEnumType, TMaskType>(TMaskType mask)
+            where TEnumType : Enum
+            where TMaskType : IBitwiseOperators<TMaskType, int, int>
+        {
+            var enumType = typeof(TEnumType);
+
+            var enumValues = new List<TEnumType>();
+
+            // Iterate the size of the mask value since:
+            // * If it is too big then we'll get an ArgumentOutOfRangeException exception.
+            // * We don't care if it is smaller than the enum's underlying type.
+            var bits = Marshal.SizeOf(typeof(TMaskType)) * 8;
+
+            for (var i = 0; i < bits; i++)
+            {
+                var bitValue = 1 << i;
+                var hasFlag = (mask & bitValue) != 0;
+
+                if (hasFlag)
+                {
+                    if (!Enum.IsDefined(enumType, bitValue))
+                    {
+                        throw new ArgumentOutOfRangeException(
+                            nameof(mask),
+                            mask,
+                            $"The value has a flag that does not correspond with the '{enumType.Name}' type.");
+                    }
+
+                    enumValues.Add((TEnumType) Enum.ToObject(enumType, bitValue));
+                }
+            }
+
+            return [.. enumValues];
+        }
+
+        /// <summary>Given an enum type with a [Flags] attribute, get an array of the enum values that have been OR'd together.</summary>
+        /// <typeparam name="TEnumType">The enumeration type.</typeparam>
+        /// <param name="flagsValue">The combined enumeration value.</param>
+        /// <returns>An array of enum values matching the values that have been OR'd together.</returns>
+        public static TEnumType[] GetValuesFromEnumWithFlags<TEnumType>(TEnumType flagsValue)
+            where TEnumType : Enum
+        {
+            var enumValues = new List<TEnumType>();
+
+            var enumCandidates = GetEnumValues<TEnumType>();
+
+            foreach (var candidate in enumCandidates)
+            {
+                if (flagsValue.HasFlag(candidate))
+                {
+                    enumValues.Add(candidate);
+                }
+            }
+
+            return [.. enumValues];
+        }
+#endif
     }
 }
