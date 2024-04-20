84e2e656fcb4bc4905bfbc276a63dc49e713fca4
Malcolm Smith <malcolm@mjfreelancing.com>
2024-04-20 19:06:46 +1000

    Cleanup

--- Source/AllOverIt/Types/VoidType.cs
+++ Source/AllOverIt/Types/VoidType.cs
@@ -1,9 +1,14 @@
 ﻿#nullable enable
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace AllOverIt.Patterns.Result
 {
+    /// <summary>Represents a void result type.</summary>
+    [ExcludeFromCodeCoverage]
     public readonly struct VoidType
     {
+        /// <summary>A static instance of <see cref="VoidType"/>.</summary>
         public static readonly VoidType Default = new();
     }
 }
