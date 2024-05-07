7f6a7607bc7ae52344b655e9c61c83f01e09d8aa
Malcolm Smith <malcolm@mjfreelancing.com>
2024-05-07 11:51:47 +1000

    nullable reference update

--- Source/AllOverIt/Collections/ReadOnlyDictionary.cs
+++ Source/AllOverIt/Collections/ReadOnlyDictionary.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 
 namespace AllOverIt.Collections
@@ -64,7 +63,9 @@ namespace AllOverIt.Collections
         }
 
         /// <inheritdoc />
-        public bool TryGetValue(TKey key, [MaybeNull] out TValue value)
+#pragma warning disable CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member (possibly because of nullability attributes).
+        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
+#pragma warning restore CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member (possibly because of nullability attributes).
         {
             return _dictionary.TryGetValue(key, out value);
         }
