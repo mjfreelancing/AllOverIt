0a46d969142bb33929a038147f68311d43093066
Malcolm Smith <malcolm@mjfreelancing.com>
2024-05-13 20:12:50 +1000

    Logging testing updates

--- Source/AllOverIt.Logging.Testing/Extensions/MethodCallsWithArgumentsExtensions.cs
+++ Source/AllOverIt.Logging.Testing/Extensions/MethodCallsWithArgumentsExtensions.cs
@@ -7,73 +7,73 @@ namespace AllOverIt.Logging.Testing
 {
     public static class MethodCallsWithArgumentsExtensions
     {
-        public static void AssertStaticLogCallEntry(this MethodCallsWithArguments methodCallsWithArguments, int index, string callerMethodName,
+        public static void AssertStaticLogCallEntry(this MethodCallContext methodCallContext, int index, string callerMethodName,
             LogLevel logLevel)
         {
             var expected = LogCallExpectation.GetExpectedStaticLogCallEntries(callerMethodName);
 
-            methodCallsWithArguments.LogLevels[index].Should().Be(logLevel);
-            methodCallsWithArguments.Entries[index].Should().BeEquivalentTo(expected);
-            methodCallsWithArguments.Exceptions[index].Should().BeNull();
+            methodCallContext.LogLevels[index].Should().Be(logLevel);
+            methodCallContext.Entries[index].Should().BeEquivalentTo(expected);
+            methodCallContext.Exceptions[index].Should().BeNull();
         }
 
-        public static void AssertStaticLogCallWithArgumentsEntry(this MethodCallsWithArguments methodCallsWithArguments, int index,
+        public static void AssertStaticLogCallWithArgumentsEntry(this MethodCallContext methodCallContext, int index,
             string callerMethodName, object arguments, LogLevel logLevel)
         {
             var expected = LogCallExpectation.GetExpectedStaticLogCallWithArgumentsEntries(callerMethodName, arguments);
 
-            methodCallsWithArguments.LogLevels[index].Should().Be(logLevel);
-            methodCallsWithArguments.Entries[index].Should().BeEquivalentTo(expected);
-            methodCallsWithArguments.Exceptions[index].Should().BeNull();
+            methodCallContext.LogLevels[index].Should().Be(logLevel);
+            methodCallContext.Entries[index].Should().BeEquivalentTo(expected);
+            methodCallContext.Exceptions[index].Should().BeNull();
         }
 
-        public static void AssertLogCallEntry<TCaller>(this MethodCallsWithArguments methodCallsWithArguments, int index, string callerMethodName,
+        public static void AssertLogCallEntry<TCaller>(this MethodCallContext methodCallContext, int index, string callerMethodName,
             LogLevel logLevel)
         {
             var expected = LogCallExpectation.GetExpectedLogCallEntries<TCaller>(callerMethodName);
 
-            methodCallsWithArguments.LogLevels[index].Should().Be(logLevel);
-            methodCallsWithArguments.Entries[index].Should().BeEquivalentTo(expected);
-            methodCallsWithArguments.Exceptions[index].Should().BeNull();
+            methodCallContext.LogLevels[index].Should().Be(logLevel);
+            methodCallContext.Entries[index].Should().BeEquivalentTo(expected);
+            methodCallContext.Exceptions[index].Should().BeNull();
         }
 
-        public static void AssertLogCallWithArgumentsEntry<TCaller>(this MethodCallsWithArguments methodCallsWithArguments, int index,
+        public static void AssertLogCallWithArgumentsEntry<TCaller>(this MethodCallContext methodCallContext, int index,
             string callerMethodName, object arguments, LogLevel logLevel)
         {
             var expected = LogCallExpectation.GetExpectedLogCallWithArgumentsEntries<TCaller>(callerMethodName, arguments);
 
-            methodCallsWithArguments.LogLevels[index].Should().Be(logLevel);
-            methodCallsWithArguments.Entries[index].Should().BeEquivalentTo(expected);
-            methodCallsWithArguments.Exceptions[index].Should().BeNull();
+            methodCallContext.LogLevels[index].Should().Be(logLevel);
+            methodCallContext.Entries[index].Should().BeEquivalentTo(expected);
+            methodCallContext.Exceptions[index].Should().BeNull();
         }
 
-        public static void AssertExceptionLogEntry(this MethodCallsWithArguments methodCallsWithArguments, int index, Exception exception)
+        public static void AssertExceptionLogEntry(this MethodCallContext methodCallContext, int index, Exception exception)
         {
             var expected = LogCallExpectation.GetExpectedExceptionLogEntries(exception);
 
-            methodCallsWithArguments.LogLevels[index].Should().Be(LogLevel.Error);
-            methodCallsWithArguments.Entries[index].Should().BeEquivalentTo(expected);
-            methodCallsWithArguments.Exceptions[index].Should().BeSameAs(exception);
+            methodCallContext.LogLevels[index].Should().Be(LogLevel.Error);
+            methodCallContext.Entries[index].Should().BeEquivalentTo(expected);
+            methodCallContext.Exceptions[index].Should().BeSameAs(exception);
         }
 
-        public static void AssertMessageEntry(this MethodCallsWithArguments methodCallsWithArguments, int index, string message,
+        public static void AssertMessageEntry(this MethodCallContext methodCallContext, int index, string message,
             LogLevel logLevel)
         {
             var expected = LogCallExpectation.GetExpectedLogTemplateEntries(message);
 
-            methodCallsWithArguments.LogLevels[index].Should().Be(logLevel);
-            methodCallsWithArguments.Entries[index].Should().BeEquivalentTo(expected);
-            methodCallsWithArguments.Exceptions[index].Should().BeNull();
+            methodCallContext.LogLevels[index].Should().Be(logLevel);
+            methodCallContext.Entries[index].Should().BeEquivalentTo(expected);
+            methodCallContext.Exceptions[index].Should().BeNull();
         }
 
-        public static void AssertMessageWithArgumentsEntry(this MethodCallsWithArguments methodCallsWithArguments, int index, string logTemplate,
+        public static void AssertMessageWithArgumentsEntry(this MethodCallContext methodCallContext, int index, string logTemplate,
             object arguments, LogLevel logLevel)
         {
             var expected = LogCallExpectation.GetExpectedLogTemplateWithArgumentsEntries(logTemplate, arguments);
 
-            methodCallsWithArguments.LogLevels[index].Should().Be(logLevel);
-            methodCallsWithArguments.Entries[index].Should().BeEquivalentTo(expected);
-            methodCallsWithArguments.Exceptions[index].Should().BeNull();
+            methodCallContext.LogLevels[index].Should().Be(logLevel);
+            methodCallContext.Entries[index].Should().BeEquivalentTo(expected);
+            methodCallContext.Exceptions[index].Should().BeNull();
         }
     }
 }
--- Source/AllOverIt.Logging.Testing/MethodCallContext.cs
+++ Source/AllOverIt.Logging.Testing/MethodCallContext.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using AllOverIt.Extensions;
+using Microsoft.Extensions.Logging;
 
 namespace AllOverIt.Logging.Testing
 {
-    public sealed record MethodCallContext
+    public sealed class MethodCallContext : List<MethodCallContext.Item>
     {
-        public LogLevel LogLevel { get; init; }
-        public Dictionary<string, object> Entries { get; init; }
-        public Exception Exception { get; init; }
+        public sealed record Item
+        {
+            public LogLevel LogLevel { get; init; }
+            public Dictionary<string, object> Entries { get; init; }
+            public Exception Exception { get; init; }
+        }
+
+        public LogLevel[] LogLevels => this.SelectToArray(item => item.LogLevel);
+        public IDictionary<string, object>[] Entries => this.SelectToArray(item => item.Entries);
+        public Exception[] Exceptions => this.SelectToArray(item => item.Exception);
     }
 }
