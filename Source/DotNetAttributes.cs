3cde06b79d545c947e8806c38bbfd0d17cdefebd
Malcolm Smith <malcolm@mjfreelancing.com>
2024-05-18 21:37:46 +1000

    Update DotNetAttributes.cs

--- Source/DotNetAttributes.cs
+++ Source/DotNetAttributes.cs
@@ -1,5 +1,3 @@
-#if !NET8_0_OR_GREATER
-
 #nullable enable
 
 using System.ComponentModel;
@@ -8,16 +6,21 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace System.Runtime.CompilerServices
 {
+#if !NET5_0_OR_GREATER
     [ExcludeFromCodeCoverage, DebuggerNonUserCode, EditorBrowsable(EditorBrowsableState.Never)]
     internal static class IsExternalInit
     {
     }
+#endif
 
+#if !NET7_0_OR_GREATER
     [ExcludeFromCodeCoverage, AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
     internal sealed class RequiredMemberAttribute : Attribute
     {
     }
+#endif
 
+#if !NET7_0_OR_GREATER
     [ExcludeFromCodeCoverage, AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
     internal sealed class CompilerFeatureRequiredAttribute : Attribute
     {
@@ -31,18 +34,53 @@ namespace System.Runtime.CompilerServices
             FeatureName = featureName;
         }
     }
+#endif
+}
 
-    namespace System.Diagnostics.CodeAnalysis
+namespace System.Diagnostics.CodeAnalysis
+{
+#if !NET7_0_OR_GREATER
+    [ExcludeFromCodeCoverage, AttributeUsage(AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
+    internal sealed class SetsRequiredMembersAttribute : Attribute
     {
-        [ExcludeFromCodeCoverage, AttributeUsage(AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
-        internal sealed class SetsRequiredMembersAttribute : Attribute
-        {
-        }
     }
+#endif
+
+#if !NET5_0_OR_GREATER
+    [ExcludeFromCodeCoverage, AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+    internal sealed class MemberNotNullAttribute : Attribute
+    {
+        public MemberNotNullAttribute(string member) => Members = new[] { member };
+        public MemberNotNullAttribute(params string[] members) => Members = members;
+        public string[] Members { get; }
+    }
+#endif
+
+#if !NET5_0_OR_GREATER
+    [ExcludeFromCodeCoverage, AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+    internal sealed class MemberNotNullWhenAttribute : Attribute
+    {
+        public MemberNotNullWhenAttribute(bool returnValue, string member)
+        {
+            ReturnValue = returnValue;
+            Members = [member];
+        }
+
+        public MemberNotNullWhenAttribute(bool returnValue, params string[] members)
+        {
+            ReturnValue = returnValue;
+            Members = members;
+        }
+
+        public bool ReturnValue { get; }
+        public string[] Members { get; }
+    }
+#endif
 }
 
 namespace System.Diagnostics
 {
+#if !NET7_0_OR_GREATER
 #pragma warning disable IDE0079 // Remove unnecessary suppression
 #pragma warning disable CS1591  // Missing XML comment for publicly visible type or member
     [ExcludeFromCodeCoverage]
@@ -65,5 +103,5 @@ namespace System.Diagnostics
     }
 #pragma warning restore CS1591
 #pragma warning restore IDE0079 // Remove unnecessary suppression
-}
 #endif
+}
