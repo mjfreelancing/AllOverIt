40634827b9ee2bbc77ef7f3b59934c286e3af1a9
Malcolm Smith <malcolm@mjfreelancing.com>
2024-06-02 11:49:00 +1000

    Comment update

--- Source/AllOverIt/Extensions/TypeInfoExtensions.cs
+++ Source/AllOverIt/Extensions/TypeInfoExtensions.cs
@@ -8,7 +8,7 @@ namespace AllOverIt.Extensions
         /// <summary>Gets all <see cref="PropertyInfo"/> (property metadata) for a given <see cref="TypeInfo"/>.</summary>
         /// <param name="typeInfo">The <see cref="TypeInfo"/> to obtain all property metadata.</param>
         /// <param name="declaredOnly">If true, the metadata of properties in the declared class as well as base class(es) are returned
-        /// (if a property is overriden then only the base class <see cref="PropertyInfo"/> is returned).
+        /// (if a property is overiden then only the base class <see cref="PropertyInfo"/> is returned).
         /// If false, only property metadata of the declared type is returned.</param>
         /// <returns>The property metadata, as <see cref="PropertyInfo"/>, of a provided <see cref="TypeInfo"/>.</returns>
         /// <remarks>When class inheritance is involved, this method returns the first property found, starting at the type represented
@@ -27,7 +27,7 @@ namespace AllOverIt.Extensions
         /// <param name="propertyName">The name of the property to obtain metadata for.</param>
         /// <returns>The property metadata, as <see cref="PropertyInfo"/>, of a specified property on the provided <paramref name="typeInfo"/>.</returns>
         /// <remarks>When class inheritance is involved, this method returns the first property found, starting at the type represented
-        /// by <paramref name="typeInfo"/>. If the property is overriden, this means the base class <see cref="PropertyInfo"/> will not be
+        /// by <paramref name="typeInfo"/>. If the property is overiden, this means the base class <see cref="PropertyInfo"/> will not be
         /// returned. If you require the base class <see cref="PropertyInfo"/> then use the <see cref="GetPropertyInfo(TypeInfo,bool)"/>
         /// method.</remarks>
         public static PropertyInfo GetPropertyInfo(this TypeInfo typeInfo, string propertyName)
@@ -46,7 +46,7 @@ namespace AllOverIt.Extensions
         /// <summary>Gets all <see cref="FieldInfo"/> (field metadata) for a given <see cref="TypeInfo"/>.</summary>
         /// <param name="typeInfo">The <see cref="TypeInfo"/> to obtain all field metadata.</param>
         /// <param name="declaredOnly">If true, the metadata of properties in the declared class as well as base class(es) are returned
-        /// (if a field is overriden then only the base class <see cref="FieldInfo"/> is returned).
+        /// (if a field is overiden then only the base class <see cref="FieldInfo"/> is returned).
         /// If false, only field metadata of the declared type is returned.</param>
         /// <returns>The field metadata, as <see cref="FieldInfo"/>, of a provided <see cref="TypeInfo"/>.</returns>
         /// <remarks>When class inheritance is involved, this method returns the first field found, starting at the type represented
