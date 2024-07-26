92128240afe46a3ddf031522a4df491bc656aabd
Malcolm Smith <malcolm@mjfreelancing.com>
2024-07-27 01:30:10 +1000

    v7.26.0 - See change log for details (#138)
    * Add CreateAutoCancellingCommand() overloads to ActivatableViewModel
    
    * Cleanup
    
    * Cleanup
    
    * Added CreateAutoCancellingTokenSource() overloads to ActivatableViewModel
    
    * Package update
    
    * Diagram updates
    
    * Version bump

--- Source/AllOverIt.ReactiveUI/ActivatableViewModel.cs
+++ Source/AllOverIt.ReactiveUI/ActivatableViewModel.cs
@@ -1,5 +1,10 @@
-﻿using ReactiveUI;
+﻿using AllOverIt.Assertion;
+using AllOverIt.ReactiveUI.Factories;
+using ReactiveUI;
+using System.Reactive;
+using System.Reactive.Concurrency;
 using System.Reactive.Disposables;
+using System.Reactive.Subjects;
 
 namespace AllOverIt.ReactiveUI
 {
@@ -7,6 +12,9 @@ namespace AllOverIt.ReactiveUI
     /// is interested in Activation events.</summary>
     public abstract class ActivatableViewModel : ReactiveObject, IActivatableViewModel
     {
+        private readonly Subject<bool> _deactivating = new();
+        private CancellationTokenSource _rootCancellationTokenSource;
+
         /// <inheritdoc />
         public ViewModelActivator Activator { get; } = new ViewModelActivator();
 
@@ -15,6 +23,10 @@ namespace AllOverIt.ReactiveUI
         {
             this.WhenActivated(disposables =>
             {
+                // Must be explicitly disposed of in OnDeactivated(). Cannot dispose via 'disposables'
+                // as it will be disposed of before OnDeactivated() is called.
+                _rootCancellationTokenSource = new();
+
                 OnActivated(disposables);
 
                 Disposable.Create(OnDeactivated).DisposeWith(disposables);
@@ -34,6 +46,84 @@ namespace AllOverIt.ReactiveUI
         /// <summary>Override this in the concrete ViewModel to be notified when it is deactivated.</summary>
         protected virtual void OnDeactivated()
         {
+            _deactivating.OnNext(true);
+
+            _rootCancellationTokenSource.Cancel();
+            _rootCancellationTokenSource.Dispose();
+            _rootCancellationTokenSource = null;
+        }
+
+        /// <summary>Creates a new <see cref="CancellationTokenSource"/> that will be automatically cancelled, if not already cancelled,
+        /// when <see cref="OnDeactivated"/> is called.</summary>
+        /// <returns>A new <see cref="CancellationTokenSource"/> that will be automatically cancelled, if not already cancelled,
+        /// when <see cref="OnDeactivated"/> is called.</returns>
+        protected CancellationTokenSource CreateAutoCancellingTokenSource()
+        {
+            return CreateAutoCancellingTokenSource(CancellationToken.None);
+        }
+
+        /// <summary>Creates a new <see cref="CancellationTokenSource"/> that will be linked with the provided <paramref name="cancellationToken"/>
+        /// and automatically cancelled, if not already cancelled, when <see cref="OnDeactivated"/> is called.</summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Anew <see cref="CancellationTokenSource"/> that will be linked with the provided <paramref name="cancellationToken"/>
+        /// and automatically cancelled, if not already cancelled, when <see cref="OnDeactivated"/> is called.</returns>
+        protected CancellationTokenSource CreateAutoCancellingTokenSource(CancellationToken cancellationToken)
+        {
+            Throw<InvalidOperationException>.WhenNull(_rootCancellationTokenSource, "Cannot create an auto-cancelling token source before activation or after deactivation.");
+
+            return CancellationTokenSource.CreateLinkedTokenSource(_rootCancellationTokenSource.Token, cancellationToken);
+        }
+
+        /// <summary>Creates a <c>ReactiveCommand&lt;Unit, Unit&gt;</c> that will be automatically cancelled, if running, when <see cref="OnDeactivated"/> is called.</summary>
+        /// <param name="action">The action to execute when the command is executed.</param>
+        /// <param name="canExecute">An optional observable that determines if the command can be executed.</param>
+        /// <param name="outputScheduler">An optional scheduler that is used to surface events. Defaults to <c>RxApp.MainThreadScheduler</c>.</param>
+        /// <returns>A <c>ReactiveCommand&lt;Unit, Unit&gt;</c> that will be automatically cancelled, if running, when <see cref="OnDeactivated"/> is called.</returns>
+        protected ReactiveCommand<Unit, Unit> CreateAutoCancellingCommand(Func<CancellationToken, Task> action, IObservable<bool> canExecute = default,
+            IScheduler outputScheduler = default)
+        {
+            _ = action.WhenNotNull(nameof(action));
+
+            return CommandFactory.CreateCancellableCommand(action, () => _deactivating, canExecute, outputScheduler);
+        }
+
+        /// <summary>Creates a <c>ReactiveCommand&lt;Unit, TResult&gt;</c> that will be automatically cancelled, if running, when <see cref="OnDeactivated"/> is called.</summary>
+        /// <param name="action">The action to execute when the command is executed.</param>
+        /// <param name="canExecute">An optional observable that determines if the command can be executed.</param>
+        /// <param name="outputScheduler">An optional scheduler that is used to surface events. Defaults to <c>RxApp.MainThreadScheduler</c>.</param>
+        /// <returns>A <c>ReactiveCommand&lt;Unit, TResult&gt;</c> that will be automatically cancelled, if running, when <see cref="OnDeactivated"/> is called.</returns>
+        protected ReactiveCommand<Unit, TResult> CreateAutoCancellingCommand<TResult>(Func<CancellationToken, Task<TResult>> action,
+            IObservable<bool> canExecute = default, IScheduler outputScheduler = default)
+        {
+            _ = action.WhenNotNull(nameof(action));
+
+            return CommandFactory.CreateCancellableCommand(action, () => _deactivating, canExecute, outputScheduler);
+        }
+
+        /// <summary>Creates a <c>ReactiveCommand&lt;TParam, Unit&gt;</c> that will be automatically cancelled, if running, when <see cref="OnDeactivated"/> is called.</summary>
+        /// <param name="action">The action to execute when the command is executed.</param>
+        /// <param name="canExecute">An optional observable that determines if the command can be executed.</param>
+        /// <param name="outputScheduler">An optional scheduler that is used to surface events. Defaults to <c>RxApp.MainThreadScheduler</c>.</param>
+        /// <returns>A <c>ReactiveCommand&lt;TParam, Unit&gt;</c> that will be automatically cancelled, if running, when <see cref="OnDeactivated"/> is called.</returns>
+        protected ReactiveCommand<TParam, Unit> CreateAutoCancellingCommand<TParam>(Func<TParam, CancellationToken, Task> action,
+            IObservable<bool> canExecute = default, IScheduler outputScheduler = default)
+        {
+            _ = action.WhenNotNull(nameof(action));
+
+            return CommandFactory.CreateCancellableCommand(action, () => _deactivating, canExecute, outputScheduler);
+        }
+
+        /// <summary>Creates a <c>ReactiveCommand&lt;TParam, TResult&gt;</c> that will be automatically cancelled, if running, when <see cref="OnDeactivated"/> is called.</summary>
+        /// <param name="action">The action to execute when the command is executed.</param>
+        /// <param name="canExecute">An optional observable that determines if the command can be executed.</param>
+        /// <param name="outputScheduler">An optional scheduler that is used to surface events. Defaults to <c>RxApp.MainThreadScheduler</c>.</param>
+        /// <returns>A <c>ReactiveCommand&lt;TParam, TResult&gt;</c> that will be automatically cancelled, if running, when <see cref="OnDeactivated"/> is called.</returns>
+        protected ReactiveCommand<TParam, TResult> CreateAutoCancellingCommand<TParam, TResult>(Func<TParam, CancellationToken, Task<TResult>> action,
+            IObservable<bool> canExecute = default, IScheduler outputScheduler = default)
+        {
+            _ = action.WhenNotNull(nameof(action));
+
+            return CommandFactory.CreateCancellableCommand(action, () => _deactivating, canExecute, outputScheduler);
         }
     }
 }
--- Tests/AllOverIt.ReactiveUI.Tests/ActivatableViewModelFixture.cs
+++ Tests/AllOverIt.ReactiveUI.Tests/ActivatableViewModelFixture.cs
@@ -1,5 +1,8 @@
 ﻿using AllOverIt.Fixture;
+using AllOverIt.Fixture.Extensions;
 using FluentAssertions;
+using ReactiveUI;
+using System.Reactive;
 using System.Reactive.Disposables;
 
 namespace AllOverIt.ReactiveUI.Tests
@@ -28,32 +31,368 @@ namespace AllOverIt.ReactiveUI.Tests
 
                 _deactivated?.Invoke();
             }
-        }
 
-        [Fact]
-        public void Should_Activate()
-        {
-            var isActivated = false;
-
-            var viewModel = new ViewModelDummy(() => { isActivated = true; }, null);
-
-            viewModel.Activator.Activate();
-
-            isActivated.Should().BeTrue();
-        }
-
-        [Fact]
-        public void Should_Deactivate()
-        {
-            var isDeactivated = false;
-
-            var viewModel = new ViewModelDummy(null, () => { isDeactivated = true; });
-
-            using (viewModel.Activator.Activate())
+            public CancellationTokenSource DoCreateAutoCancellingTokenSource()
             {
+                return CreateAutoCancellingTokenSource();
             }
 
-            isDeactivated.Should().BeTrue();
+            public CancellationTokenSource DoCreateAutoCancellingTokenSource(CancellationToken cancellationToken)
+            {
+                return CreateAutoCancellingTokenSource(cancellationToken);
+            }
+
+            public ReactiveCommand<Unit, Unit> DoCreateAutoCancellingCommand(Func<CancellationToken, Task> action)
+            {
+                return CreateAutoCancellingCommand(action);
+            }
+
+            public ReactiveCommand<Unit, TResult> DoCreateAutoCancellingCommand<TResult>(Func<CancellationToken, Task<TResult>> action)
+            {
+                return CreateAutoCancellingCommand<TResult>(action);
+            }
+
+            public ReactiveCommand<TParam, Unit> DoCreateAutoCancellingCommand<TParam>(Func<TParam, CancellationToken, Task> action)
+            {
+                return CreateAutoCancellingCommand<TParam>(action);
+            }
+
+            public ReactiveCommand<TParam, TResult> DoCreateAutoCancellingCommand<TParam, TResult>(Func<TParam, CancellationToken, Task<TResult>> action)
+            {
+                return CreateAutoCancellingCommand<TParam, TResult>(action);
+            }
+        }
+
+        public class OnActivated : ActivatableViewModelFixture
+        {
+            [Fact]
+            public void Should_Activate()
+            {
+                var isActivated = false;
+
+                var viewModel = new ViewModelDummy(() => { isActivated = true; }, null);
+
+                viewModel.Activator.Activate();
+
+                isActivated.Should().BeTrue();
+            }
+        }
+
+        public class OnDeactivated : ActivatableViewModelFixture
+        {
+            [Fact]
+            public void Should_Deactivate()
+            {
+                var isDeactivated = false;
+
+                var viewModel = new ViewModelDummy(null, () => { isDeactivated = true; });
+
+                using (viewModel.Activator.Activate())
+                {
+                }
+
+                isDeactivated.Should().BeTrue();
+            }
+        }
+
+        public class CreateAutoCancellingTokenSource : ActivatableViewModelFixture
+        {
+            [Fact]
+            public void Should_Throw_Before_Activation()
+            {
+                var viewModel = new ViewModelDummy(null, null);
+
+                Invoking(() =>
+                {
+                    _ = viewModel.DoCreateAutoCancellingTokenSource();
+                })
+                .Should()
+                .Throw<InvalidOperationException>()
+                .WithMessage("Cannot create an auto-cancelling token source before activation or after deactivation.");
+            }
+
+            [Fact]
+            public void Should_Throw_After_Deactivation()
+            {
+                var viewModel = new ViewModelDummy(null, null);
+
+                using (viewModel.Activator.Activate())
+                {
+                }
+
+                Invoking(() =>
+                {
+                    _ = viewModel.DoCreateAutoCancellingTokenSource();
+                })
+                .Should()
+                .Throw<InvalidOperationException>()
+                .WithMessage("Cannot create an auto-cancelling token source before activation or after deactivation.");
+            }
+
+            [Fact]
+            public void Should_Auto_Cancel_Token_Source()
+            {
+                var viewModel = new ViewModelDummy(null, null);
+
+                var cts = new CancellationTokenSource[3];
+
+                using (viewModel.Activator.Activate())
+                {
+                    cts[0] = viewModel.DoCreateAutoCancellingTokenSource();
+                    cts[1] = viewModel.DoCreateAutoCancellingTokenSource();
+                    cts[2] = viewModel.DoCreateAutoCancellingTokenSource();
+
+                    cts[0].IsCancellationRequested.Should().BeFalse();
+                    cts[1].IsCancellationRequested.Should().BeFalse();
+                    cts[2].IsCancellationRequested.Should().BeFalse();
+                }
+
+                cts[0].IsCancellationRequested.Should().BeTrue();
+                cts[1].IsCancellationRequested.Should().BeTrue();
+                cts[2].IsCancellationRequested.Should().BeTrue();
+
+                cts[0].Dispose();
+                cts[1].Dispose();
+                cts[2].Dispose();
+            }
+        }
+
+        public class CreateAutoCancellingTokenSource_CancellingToken : ActivatableViewModelFixture
+        {
+            [Fact]
+            public void Should_Throw_Before_Activation()
+            {
+                var viewModel = new ViewModelDummy(null, null);
+
+                Invoking(() =>
+                {
+                    _ = viewModel.DoCreateAutoCancellingTokenSource(CancellationToken.None);
+                })
+                .Should()
+                .Throw<InvalidOperationException>()
+                .WithMessage("Cannot create an auto-cancelling token source before activation or after deactivation.");
+            }
+
+            [Fact]
+            public void Should_Throw_After_Deactivation()
+            {
+                var viewModel = new ViewModelDummy(null, null);
+
+                using (viewModel.Activator.Activate())
+                {
+                }
+
+                Invoking(() =>
+                {
+                    _ = viewModel.DoCreateAutoCancellingTokenSource(CancellationToken.None);
+                })
+                .Should()
+                .Throw<InvalidOperationException>()
+                .WithMessage("Cannot create an auto-cancelling token source before activation or after deactivation.");
+            }
+
+            [Fact]
+            public void Should_Auto_Cancel_Token_Source()
+            {
+                var viewModel = new ViewModelDummy(null, null);
+
+                using var otherCts1 = new CancellationTokenSource();
+                using var otherCts2 = new CancellationTokenSource();
+                using var otherCts3 = new CancellationTokenSource();
+
+                var cts = new CancellationTokenSource[3];
+
+                using (viewModel.Activator.Activate())
+                {
+                    cts[0] = viewModel.DoCreateAutoCancellingTokenSource(otherCts1.Token);
+                    cts[1] = viewModel.DoCreateAutoCancellingTokenSource(otherCts2.Token);
+                    cts[2] = viewModel.DoCreateAutoCancellingTokenSource(otherCts3.Token);
+
+                    otherCts1.IsCancellationRequested.Should().BeFalse();
+                    otherCts2.IsCancellationRequested.Should().BeFalse();
+                    otherCts3.IsCancellationRequested.Should().BeFalse();
+
+                    cts[0].IsCancellationRequested.Should().BeFalse();
+                    cts[1].IsCancellationRequested.Should().BeFalse();
+                    cts[2].IsCancellationRequested.Should().BeFalse();
+                }
+
+                // These will still be non-cancelled
+                otherCts1.IsCancellationRequested.Should().BeFalse();
+                otherCts2.IsCancellationRequested.Should().BeFalse();
+                otherCts3.IsCancellationRequested.Should().BeFalse();
+
+                // But the linked tokens will be cancelled
+                cts[0].IsCancellationRequested.Should().BeTrue();
+                cts[1].IsCancellationRequested.Should().BeTrue();
+                cts[2].IsCancellationRequested.Should().BeTrue();
+
+                cts[0].Dispose();
+                cts[1].Dispose();
+                cts[2].Dispose();
+            }
+        }
+
+        public class CreateAutoCancellingCommand_Unit_Unit : ActivatableViewModelFixture
+        {
+            [Fact]
+            public void Should_Throw_When_Action_Null()
+            {
+                var viewModel = new ViewModelDummy(null, null);
+
+                Invoking(() =>
+                {
+                    _ = viewModel.DoCreateAutoCancellingCommand((Func<CancellationToken, Task>) null);
+                })
+                .Should()
+                .Throw<ArgumentNullException>()
+                .WithNamedMessageWhenNull("action");
+            }
+
+            [Fact]
+            public void Should_Auto_Cancel_Command()
+            {
+                var viewModel = new ViewModelDummy(null, null);
+
+                var command = viewModel.DoCreateAutoCancellingCommand(cancellationToken => Task.Delay(-1, cancellationToken));
+
+                var isExecutingList = new List<bool>();
+
+                var subscription1 = command.IsExecuting.Subscribe(isExecuting => isExecutingList.Add(isExecuting));
+
+                using var subscription2 = command.Execute().Subscribe();
+
+                using (viewModel.Activator.Activate())
+                {
+                    isExecutingList.Should().BeEquivalentTo([false, true], options => options.WithStrictOrdering());
+                }
+
+                isExecutingList.Should().BeEquivalentTo([false, true, false], options => options.WithStrictOrdering());
+            }
+        }
+
+        public class CreateAutoCancellingCommand_Unit_Result : ActivatableViewModelFixture
+        {
+            [Fact]
+            public void Should_Throw_When_Action_Null()
+            {
+                var viewModel = new ViewModelDummy(null, null);
+
+                Invoking(() =>
+                {
+                    _ = viewModel.DoCreateAutoCancellingCommand((Func<CancellationToken, Task<string>>) null);
+                })
+                .Should()
+                .Throw<ArgumentNullException>()
+                .WithNamedMessageWhenNull("action");
+            }
+
+            [Fact]
+            public void Should_Auto_Cancel_Command()
+            {
+                var viewModel = new ViewModelDummy(null, null);
+
+                var command = viewModel.DoCreateAutoCancellingCommand<string>(async cancellationToken =>
+                {
+                    await Task.Delay(-1, cancellationToken);
+
+                    return Create<string>();
+                });
+
+                var isExecutingList = new List<bool>();
+
+                var subscription1 = command.IsExecuting.Subscribe(isExecuting => isExecutingList.Add(isExecuting));
+
+                using var subscription2 = command.Execute().Subscribe();
+
+                using (viewModel.Activator.Activate())
+                {
+                    isExecutingList.Should().BeEquivalentTo([false, true], options => options.WithStrictOrdering());
+                }
+
+                isExecutingList.Should().BeEquivalentTo([false, true, false], options => options.WithStrictOrdering());
+            }
+        }
+
+        public class CreateAutoCancellingCommand_Param_Unit : ActivatableViewModelFixture
+        {
+            [Fact]
+            public void Should_Throw_When_Action_Null()
+            {
+                var viewModel = new ViewModelDummy(null, null);
+
+                Invoking(() =>
+                {
+                    _ = viewModel.DoCreateAutoCancellingCommand<string>((Func<string, CancellationToken, Task>) null);
+                })
+                .Should()
+                .Throw<ArgumentNullException>()
+                .WithNamedMessageWhenNull("action");
+            }
+
+            [Fact]
+            public void Should_Auto_Cancel_Command()
+            {
+                var viewModel = new ViewModelDummy(null, null);
+
+                var command = viewModel.DoCreateAutoCancellingCommand<string>((value, cancellationToken) => Task.Delay(-1, cancellationToken));
+
+                var isExecutingList = new List<bool>();
+
+                var subscription1 = command.IsExecuting.Subscribe(isExecuting => isExecutingList.Add(isExecuting));
+
+                using var subscription2 = command.Execute().Subscribe();
+
+                using (viewModel.Activator.Activate())
+                {
+                    isExecutingList.Should().BeEquivalentTo([false, true], options => options.WithStrictOrdering());
+                }
+
+                isExecutingList.Should().BeEquivalentTo([false, true, false], options => options.WithStrictOrdering());
+            }
+        }
+
+        public class CreateAutoCancellingCommand_Param_Result : ActivatableViewModelFixture
+        {
+            [Fact]
+            public void Should_Throw_When_Action_Null()
+            {
+                var viewModel = new ViewModelDummy(null, null);
+
+                Invoking(() =>
+                {
+                    _ = viewModel.DoCreateAutoCancellingCommand<string, int>((Func<string, CancellationToken, Task<int>>) null);
+                })
+                .Should()
+                .Throw<ArgumentNullException>()
+                .WithNamedMessageWhenNull("action");
+            }
+
+            [Fact]
+            public void Should_Auto_Cancel_Command()
+            {
+                var viewModel = new ViewModelDummy(null, null);
+
+                var command = viewModel.DoCreateAutoCancellingCommand<string, int>(async (value, cancellationToken) =>
+                {
+                    await Task.Delay(-1, cancellationToken);
+
+                    return Create<int>();
+                });
+
+                var isExecutingList = new List<bool>();
+
+                var subscription1 = command.IsExecuting.Subscribe(isExecuting => isExecutingList.Add(isExecuting));
+
+                using var subscription2 = command.Execute().Subscribe();
+
+                using (viewModel.Activator.Activate())
+                {
+                    isExecutingList.Should().BeEquivalentTo([false, true], options => options.WithStrictOrdering());
+                }
+
+                isExecutingList.Should().BeEquivalentTo([false, true, false], options => options.WithStrictOrdering());
+            }
         }
     }
 }
